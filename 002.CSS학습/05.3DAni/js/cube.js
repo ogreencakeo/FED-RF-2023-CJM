45a32b310a6380ddf4dd2363a7656d7174920cd3
ogreencakeo <magente2000@gmail.com>
2023-09-01 11:21:07 +0900

    .

--- 002.CSS학습/05.3DAni/js/cube.js
+++ 002.CSS학습/05.3DAni/js/cube.js
@@ -10,3 +10,32 @@
     버튼으로 변경되어 있음!
 
 *************************************/
+// window로드 이벤트 호출
+window.addEventListener('DOMContentLoaded', loadFn);
+
+// DOM 선택함수
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+// 로딩함수
+function loadFn(){
+    // 함수호출 확인
+    console.log('로딩완료!');
+
+    // 1-1. 이벤트 선정 : .btngo
+    const btngo = qs('.btngo');
+    // 1-2. 변경대상 : .cube
+    const cube = qs('.cube');
+
+    console.log('대상 :', btngo, cube);
+
+    // 2. 이벤트 함수 설정하기
+    btngo.onclick = () => {
+        // 호출확인
+        console.log('돌아!');
+        // 1. 대상 : .cuble -> cube변수
+        // 2. 변경내용 : 클래스 on 없으면 넣고 있으면 빼기
+        // classList.toggle(클래스명)
+        cube.classList.toggle("on");
+    };
+}
