84b511b7093f941fc6ebe3118c1f637cb117a119
ogreencakeo <magente2000@gmail.com>
2023-09-07 12:44:39 +0900

    Update main.js

--- 001.HTML학습/09.ShoppingMall_PJ/js/main.js
+++ 001.HTML학습/09.ShoppingMall_PJ/js/main.js
@@ -43,7 +43,15 @@ addEvt(window, "DOMContentLoaded", loadFn);
 // 전역변수구역 //
 // 1. 광클금지상태변수 : 0 - 허용, 1 - 불허용
 let clickSts = 0;
+// 2. 슬라이드 이동시간 : 상수로 설정
+const TIME_SLIDE = 400;
 
+/*
+    (참고 : JS에서 이름짓는 일반규칙)
+    1. 변수/함수 : 캐믈케이스(첫단어 소문자 뒷단어 대문자 시작)
+    2. 생성자함수/크래스 : 파스칼케이스(모든 첫글자 대문자)
+    3. 상수 : 모든 글자 대문자(연결은 언더스코어 - 스네이크 케이스)
+*/
 
 /****************************************** 
     함수명: loadFn
@@ -69,7 +77,7 @@ function loadFn() {
         // 광클금지
         if(clickSts) return; // 나가!
         clickSts = 1; // 잠금!
-        setTimeout(()=>clickSts = 0,400); // 해제
+        setTimeout(()=>clickSts = 0,TIME_SLIDE); // 해제
 
         // 호출확인
         console.log("나야나!", this, this.classList.contains("ab2"));
@@ -90,7 +98,7 @@ function loadFn() {
             slide.style.left = '-100%';
 
             // 2. 트랜지션 주기
-            slide.style.transition = '.4s ease-in-out';
+            slide.style.transition = TIME_SLIDE+'ms ease-in-out';
 
             // 이동시간 후 맨앞 li 잘라서 맨뒤로 이동하기
             // appendChild(요소)
@@ -103,7 +111,7 @@ function loadFn() {
 
                 // 5. 트랜지션 없애기
                 slide.style.transition = 'none';
-            }, 400);
+            }, TIME_SLIDE);
         }else{  // 왼쪽 버튼
             // 1. 맨뒤 li 맨 앞으로 이동
             // 놈.놈.놈 -> > insertBefore(넣을놈, 넣을놈전놈)
@@ -125,7 +133,7 @@ function loadFn() {
                 slide.style.left = '0%';
     
                 // 4. 트랜지션 주기
-                slide.style.transition = '.4s ease-in-out'
+                slide.style.transition = TIME_SLIDE+'ms ease-in-out'
             }, 0);
 
 
