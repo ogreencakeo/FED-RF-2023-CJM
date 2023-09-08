dc02ee8e4f80d8092955f6ba8b151e9b89f9a584
ogreencakeo <magente2000@gmail.com>
2023-09-08 11:09:39 +0900

    .

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -1,5 +1,20 @@
 // CGV PJ 메인 페이지 JS - main.js
 
-// 요소선택함수 ////////
-const qs = (x) => document.querySelector(x);
-const qsa = (x) => document.querySelectorAll(x);
+// DOM 함수 객체
+const domFn = {
+    // 요소선택함수 ////////
+    qs : (x) => document.querySelector(x),
+    qsa : (x) => document.querySelectorAll(x),
+    
+    // 이벤트셋팅함수
+    addEvt : (ele, evt, fn) => ele.addEventListener(evt, fn),
+};  // domFn 객체
+
+// 구현1. 
+// [ 포스터 메뉴를 클릭하여 메인 유튜브 이미지를 변경한다! ]
+// 1. 대상
+// 1-1. 이벤트 대상 : .poster-menu a
+const pmenu = domFn.qsa('.poster-menu a');
+// 1-2. 변경 대상 : .screen
+const screen = domFn.qs('.screen');
+console.log('대상(pmenu) :', pmenu, '대상(screen) :',screen);
