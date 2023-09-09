05013d3eeb289f1227f5b30a83d02872e9668a4f
jimin0427 <cjm0427@hs.ac.kr>
2023-09-09 20:09:27 +0900

    .
    .

--- 001.HTML학습/09.ShoppingMall_PJ copy/js/pra.js
+++ 001.HTML학습/09.ShoppingMall_PJ copy/js/pra.js
@@ -4,23 +4,46 @@
 const qs = (x) => document.querySelector(x);
 const qsa = (x) => document.querySelectorAll(x);
 
-// addEvent 함수
-// ele - 요소, evt - 이벤트, fn - 함수
 const addEvt = 
 (ele, evt, fn) => ele.addEventListener(evt, fn);
 
-let clickSts = 0;
-const TIME_SLIDE = 400;
-
-// HTML태그 로딩후 loadFn함수 호출! ///
 addEvt(window,"DOMContentLoaded", loadFn);
-
 function loadFn() {
-    
-    
-    
+    console.log("로딩완료!");
+
+    let abtn = qsa('.abtn');
+    let slide = qs('#slide');
+    let indic = qsa('.indic li');
+
+    slide.querySelectorAll('li').forEach((ele, idx) => ele.setAttribute('data-seq', idx));
+
+    abtn.forEach((ele)=>addEvt(ele, 'click', goShow));
+    function goShow(){
+        let isRight = this.classList.contains('ab2');
+        let eachOne = slide.querySelectorAll('li');
+        if(isRight){
+            slide.style.left = '-100%';
+            slide.style.transition = '.4s ease-in-out';
+            setTimeout(() => {
+                slide.appendChild(eachOne[0]);
+                slide.style.left = '0';
+                slide.style.transition = 'none';
+            }, 400);
+        }else{
+            slide.style.left = '-100%';
+            slide.style.transition = 'none';
+            setTimeout(()=>{
+                slide.insertBefore(eachOne[eachOne.length-1], eachOne[0]);
+                slide.style.left = '0';
+                slide.style.transition = '.4s ease-in-out';
+            })
+        }
+        let nowSeq = slide.querySelectorAll('li')[isRight? 1 : 0].getAttribute('data-seq');
+        indic.forEach((ele, idx)=>{
+            if(nowSeq == idx) ele.classList.add('on');
+            else ele.classList.remove('on');
+        });
+    }
 
     
-
-
 } 
