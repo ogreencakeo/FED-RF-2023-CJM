535ecf5905e46f4653ce5dc0cc89dc66cca43405
ogreencakeo <magente2000@gmail.com>
2023-09-13 15:09:28 +0900

    /

--- 003.JS학습/01_JS실험실/js/scroll_action.js
+++ 003.JS학습/01_JS실험실/js/scroll_action.js
@@ -9,6 +9,9 @@ const domFn = {
 
     // 바운딩 위치값 함수
     getBCR: (ele) => ele.getBoundingClientRect().top,
+
+    // 옵셋탑값 반환함수
+    getOT : (ele) => ele.offsetTop,
 };
 
 // 부드러운 스크롤 호출
@@ -40,20 +43,28 @@ startSS();
     
 ***********************************************************/
 // 1. 대상선정
-// 스크롤 등장 대상 : .scact
-const scAct = domFn.qsa(".scact");
+// 스크롤 등장 대상 : .hide-el
+const scAct = domFn.qsa(".hide-el");
 console.log("대상(scAct) :", scAct);
 
 // 2. 전체 window에 스크롤 이벤트 셋팅하기
-// 스크롤 등장액션 이벤트 설정
+// 2-1. 스크롤 등장액션 이벤트 설정
 domFn.addEvt(window, "scroll", showIt);
-// 스크롤시 떨어지는 여자 이벤트 설정
+// 2-2. 스크롤시 떨어지는 여자 이벤트 설정
 domFn.addEvt(window, "scroll", moveWoman);
+// 2-3. 스크롤시 타이틀 이동 애니 이벤트 설정
+domFn.addEvt(window, "scroll", moveTit);
 
 // 요소 위치값
 // let pos1 = scAct[0].offsetTop;
 // let pos2 = scAct[1].offsetTop;
 // let pos3 = scAct[2].offsetTop;
+
+// 각 요소 옵셋top값 구하기
+const posTop = [];
+
+scAct.forEach((ele, idx) => posTop[idx] = domFn.getOT(ele));
+console.log('각 위치 배열 :', posTop);
 
 // 3. 스크롤 등장 기준설정 : 화면의 3/4
 const CRITERIA = (window.innerHeight / 4) * 3;
@@ -91,6 +102,42 @@ function addOn(ele) {
     else ele.classList.remove("on");
 }
 
+// 구현내용: 글자를 박스에 넣고 하나씩 날아오면서 등장
+// 1.대상선정: .stage
+const stage = domFn.qs(".stage");
+// console.log(stage);
+
+// 2.데이터 변수할당
+const mytxt = "신카이 마코토";
+
+// 3.데이터글자 한글자씩 태그싸기
+// for of문사용!
+
+let hcode = ""; // 코드저장변수
+let idx = 0; // 순번변수
+
+for (let x of mytxt) {
+    // 띄어쓰기일 경우 특수문자처리!
+    if (x === " ") hcode += "&nbsp";
+    // 코드만들어 변수에 대입연산자로 넣기!
+    else
+        hcode += `<span style="transition-delay: ${idx * 0.1}s">
+                    ${x}</span>`;
+
+    // 순번변수증가
+    idx++;
+} //////////// for of ////////////
+
+// console.log(hcode);
+
+// 4. 스테이지 박스에 글자넣기
+stage.innerHTML = hcode;
+
+// 5. 일정시간후 스테이지박스에 클래스 "on"주고 애니작동!
+setTimeout(() => {
+    stage.classList.add("on");
+}, 2000);
+
 // 스크롤시 떨어지는 여자 함수
 // 원리 : 전체 페이지 스크롤 이동한계값을 기준으로 비례식을 세워
 // 보이는 화면에서의 떨녀의 위치를 정해줌
@@ -116,11 +163,43 @@ function moveWoman() {
     // 이미지 이동값 = 윈도우높이 * 스크롤이동값 / 스크롤한계값
     let wTop = (winH * scTop) / scLimit;
 
-    console.log("난 떨녀! scTop :", scTop, ", wTop :", wTop);
+    // console.log("난 떨녀! scTop :", scTop, ", wTop :", wTop);
 
     // 3. 떨어지는 여자에 적용하기
     woman.style.top = wTop + "px";
 
     // 4. 맨위일때 윗쪽으로 숨기기
     if (scTop == 0) woman.style.top = "-20%";
+}
+
+// 타이틀 이동 애니 함수
+// 대상 : .tit
+const tit = domFn.qs('.tit');
+
+function moveTit(){
+    // 스크롤 위치값
+    let scTop = window.scrollY;
+    // console.log('타이틀이야!', scTop);
+
+    if(scTop > posTop[0] - winH/2 && scTop < posTop[0]){
+        tit.style.top = '50%';
+        tit.style.left = '20%';
+        tit.style.transition = '2s';
+    }
+    else if(scTop > posTop[1] - winH/2 && scTop < posTop[1]){
+        tit.style.top = '70%';
+        tit.style.left = '65%';
+        tit.style.transition = '1s';
+    }
+    else if(scTop > posTop[2] - winH/2 && scTop < posTop[2]){
+        tit.style.top = '50%';
+        tit.style.left = '25%';
+        tit.style.transition = '0.5s';
+    }
+    else{
+        tit.style.top = '0';
+        tit.style.left = '50%';
+        tit.style.transition = '1s';
+    }
+
 }
