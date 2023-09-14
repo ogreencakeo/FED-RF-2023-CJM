404d7ab1fd7c67318b451c31b4a78dda256193c5
ogreencakeo <magente2000@gmail.com>
2023-09-14 10:09:43 +0900

    Update nav06.js

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -1,29 +1,54 @@
-/* 네비게이션 유형6 JS - nav06.js */
-// 가로네비 서브별 드롭다운 전체형
+// 네비게이션 유형6 JS - nav06.js
+// 가로네비 서브별 드롭다운 전체창
 
-// DOM 함수 객체 //////////////
 const domFn = {
     // 요소선택함수 ////////
-    qs : (x) => document.querySelector(x),
-    qsEl : (el,x) => el.querySelector(x),
-    qsa : (x) => document.querySelectorAll(x),
-    qsaEl : (el,x) => el.querySelectorAll(x),
-    
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+
     // 이벤트셋팅함수
-    addEvt : (ele,evt,fn) => ele.addEventListener(evt,fn),
-}; 
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+}; /////// domFn 객체 /////////////
 
 // 1. 구현 요구사항
-// GNB메뉴의 데이터를 모두 HTML DOM으로 구조화 하여 
+// GNB메뉴의 데이터를 모두 html DOM으로 구조화하여
 // 화면에 출력한다!
 
-// 2. 대상 선정 : .gnb
-const gnbBox = domFn.qs('.gnb');
-console.log('대상 :', gnbBox);
+// 2. 대상선정: .gnb
+const gnbBox = domFn.qs(".gnb");
+console.log("대상", gnbBox);
 
-// 3. 객체 데이터로 html 코드 만들기
-let hcode = '';
-hcode += ``;
-for(let x in mdata){
-    console.log("mdata x >> ", x);
-}
+// 3. 객체 데이터로 html 코드만들기
+let hcode = "";
+
+for (let x in mdata) {
+    console.log("속성명:", x);
+    hcode += `  
+        <ul>
+            <li>
+                <a href="#">${x}</a>
+                <div class="smenu">
+                    <aside class="smbx">
+                        <h2>
+                            <div class="stit">1차</div>
+                            <a href="">전체보기</a>
+                            <div class="swrap">
+                                <dl>
+                                    <dt>2차</dt>
+                                    <dd><a href="">3차</a></dd>
+                                </dl>
+                            </div>
+                        </h2>
+                    </aside>
+                </div>
+            </li>
+        </ul>
+    `;
+} //////// for in /////////////
+
+// 확인
+console.log("코드:", hcode);
+
+// ul>li>a[href='#']{1차}+.smenu>aside.smbx>h2>(.stit{2차}+a{전체보기})+.swrap>dl>dt+dd>a{3차}
