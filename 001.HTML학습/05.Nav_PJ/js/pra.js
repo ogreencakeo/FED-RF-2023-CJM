1f1d7aab8b58a644b7b6b3dc3443e487b054f905
jimin0427 <cjm0427@hs.ac.kr>
2023-09-17 19:18:18 +0900

    f

--- 001.HTML학습/05.Nav_PJ/js/pra.js
+++ 001.HTML학습/05.Nav_PJ/js/pra.js
@@ -1,4 +1,4 @@
-/* 네비게이션 유형6 JS - nav06.js */
+d/* 네비게이션 유형6 JS - nav06.js */
 // 가로네비 서브별 드롭다운 전체형
 
 // DOM 함수 객체 //////////////
@@ -13,3 +13,43 @@ const domFn = {
     addEvt : (ele,evt,fn) => ele.addEventListener(evt,fn),
 }; 
 
+const gnbBx = domFn.qs('.gnb');
+
+let hcode = '';
+for(let x in mdata){
+    hcode += `
+        <ul>
+            <li>
+                <a href = "#">${x}</a>
+                <div class ="smenu">
+                    <aside>
+                        <h2>
+                            <div class = "stit">${x}</div>
+                            <a href = "#">전체보기</a>
+                        </h2>
+                        <div class ="swrap">
+                            ${makeCode(mdata[x])}
+                        </div>
+                    </aside>
+                </div>
+            </li>
+        </ul>
+    `;
+}
+
+function makeCode(obj){
+    let hcode = '';
+    for(let x in obj){
+        hcode += `
+            <dl>
+                <dt>${x}</dt>
+                ${obj[x].map(val=>`<dd><a href="#">${val}</a></dd>`)}
+            </dl>
+        `;
+    }
+    return hcode;
+}
+
+gnbBx.innerHTML = hcode;
+
+gnb
