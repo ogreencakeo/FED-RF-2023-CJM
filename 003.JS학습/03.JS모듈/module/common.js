cdead0cb0322cb13cadd675c6ae1b34948ed538e
ogreencakeo <magente2000@gmail.com>
2023-09-19 10:20:10 +0900

    .

--- 003.JS학습/03.JS모듈/module/common.js
+++ 003.JS학습/03.JS모듈/module/common.js
@@ -1,1 +1,18 @@
-// 공통함수 JS
+// 공통함수 JS - common.js
+
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+
+    // 바운딩위치값함수
+    getBCR: (ele) => ele.getBoundingClientRect().top,
+
+    // 옵셋탑값 반환함수
+    getOT: (ele) => ele.offsetTop,
+};
