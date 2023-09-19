2e408cb979b41b544ae98cd58a09cb233e275b6e
ogreencakeo <magente2000@gmail.com>
2023-09-19 13:06:28 +0900

    Update main.js

--- 003.JS학습/01_JS실험실/Parallax_PJ/js/main.js
+++ 003.JS학습/01_JS실험실/Parallax_PJ/js/main.js
@@ -37,6 +37,15 @@ setTimeout(() => {
     // 안하면 원래 위치로 스크롤시 튐!
 }, 400);
 
+// 0. 스크롤바 트랙을 잡고 위치 이동시 위치값 반영
+dFn.addEvt(window, 'mouseup', ()=>{
+    setPos(window.scrollY);
+});
+// 0. 키보드 방향키 이동시 위치값 반영
+dFn.addEvt(window, 'keyup', ()=>{
+    setPos(window.scrollY);
+});
+
 // 1. 대상선정
 // 1-1. 글자박스
 const txtBox = dFn.qsa('.txt');
@@ -53,7 +62,21 @@ dFn.addEvt(window, 'scroll', scrollFn);
 // 3-1. 스크롤 이벤트 함수
 function scrollFn(){
     console.log('스크롤~~~!');
+
+    // 1. 대상1 : 글자박스 패럴렉스 호출!
+    txtBox.forEach(ele => 
+
+        moveEl(dFn.getBCR(ele), ele, setH2));
+    // 2. 대상2 : 아이콘 패럴렉스 호출!
+    icon.forEach(ele => 
+        moveEl(dFn.getBCR(ele), ele, setH1));
 }
+
+// 셋팅값 변수
+// 윈도우 높이값
+const winH = window.innerHeight;
+// 패럴렉스 범위변수
+const setH1 = 100, setH2 = 200;
 
 // 3-2. 패럴렉스 이동함수
 function moveEl(elPos, ele, setH){
@@ -64,5 +87,40 @@ function moveEl(elPos, ele, setH){
     console.log(
         '위치값 :', elPos, 
         '\n대상 :', ele, 
-        '\n범위 :', setH);
+        '\n범위 :', setH
+    );
+
+    // [ 패럴렉스 범위 : 윈도우 높이값 ~ 0 ]
+    // 화면에서 완전히 사라질때 범위는 0보다 작다 (약간의 마이너스 값)
+    if(elPos < winH && elPos > -200){
+        // 1. 위치 이동값 계산
+        // 실제이동값 = 위치값 * 정한범위 / 전체범위
+        let x = setH - (elPos*setH / winH);
+        console.log('x :', -x);
+
+        // 2. 해당요소의 위치값 이동 CSS반영하기
+        // Y축 이동시 윗쪽 방향은 마이너스임! : -x
+        ele.style.transform = `
+            translateY(${-x}px)
+        `;
+    }
+
+    /*********************************************************
+        [ 패럴렉스 위치 계산 ]
+        1. 전체범위 : 윈도우 높이값
+        2. 위치값 : getBoundingClientRect().top
+        3. 정한 범위 : 이동할 수치
+        4. 실제 이동값 : transform : translateY(이동수치 px)
+        _________________________________________________
+
+        ( 비례식으로 실제 이동값 알아내기 )
+        전체범위 : 위치값 = 정한범위 : 실제이동값
+        실제이동값 = 위치값 * 정한범위 / 전체범위
+
+        -> 그.런.데...
+        Y축 위치이동은 처음에 0부터 서서히 커지므로 
+        이동수치값은 정한범위에서 실제 이동값을 빼야함!
+
+        실제이동값 = 정한범위 - (위치값 * 정한범위 / 전체범위)
+    *********************************************************/
 }
