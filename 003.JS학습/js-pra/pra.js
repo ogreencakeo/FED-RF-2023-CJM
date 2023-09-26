473f9ef9a8cc0f651f3dda2e54748000cadbb06e
jimin0427 <cjm0427@hs.ac.kr>
2023-09-26 21:51:29 +0900

    ,

--- 003.JS학습/js-pra/pra.js
+++ 003.JS학습/js-pra/pra.js
@@ -1,67 +1,58 @@
 import dFn from './dom.js';
-console.log(dFn);
 
-const fruit = ['배', '사과', '용과', '딸기'];
+const tt = dFn.qsa('.tt');
 
-const frObj = {
-    배: "fruits_01",
-    사과: "fruits_02",
-    용과: "fruits_14",
-    딸기: "fruits_09",
-    두리안: "fruits_17",
-    바나나: "fruits_15",
-    수박: "fruits_12",
-    파인애플: "fruits_13",
-    망고: "fruits_24",
-    오렌지: "fruits_03",
-    체리: "fruits_05",
-    멜론: "fruits_11",
-    블루베리: "fruits_20",
-    레몬: "fruits_04",
-};
+const week = ['일', '월', '화', '수', '목', '금', '토'];
+const addZero = (num) => (num<10? "0"+num : num);
 
-const mbtn = dFn.qsa('.mbtn');
-const showit = dFn.qs('.showit');
-const cont = dFn.qs('.cont');
-const sel = dFn.qs('#sel');
-const anum = dFn.qs('#anum');
-const delnum = dFn.qs('#delnum');
+setInterval(showTime, 1000);
+showTime();
 
-const showArray = () => showit.innerText = fruit.join('/');
-showArray();
-const bindCombo = () => anum.innerHTML = fruit.map((ele, idx) => `<option value = "${idx}">${ele}</option>`).join("");
+function showTime(){
+    const today = new Date();
+    tt[0].innerText = today.getFullYear();
+    tt[1].innerText = today.getMonth() + 1;
+    tt[2].innerText = today.getDate();
+    tt[3].innerText = week[today.getDay()];
 
-sel.innerHTML = Object.keys(frObj).map(v => `<option>${v}</option>`).join("");
+    let H = today.getHours();
+    tt[5].innerText = addZero(H>12? H-12 : H);
+    tt[4].innerText = H>12? '오후':'오전';
 
-mbtn.forEach(ele => dFn.addEvt(ele, 'click', showFruit));
+    let M = today.getMinutes();
+    tt[6].innerText = addZero(M);
 
-function showFruit(){
-    let btxt = this.innerText;
-    if(btxt == '과일주세요~!'){
-        let hcode = '<ul>';
-        fruit.forEach(ele => {
-            hcode += `
-                <li style = 'background : url(../addimg/${frObj[ele]}).png no-repeat center/cover'>
-                    ${ele}
-                </li>
-            `;
-        });
-        hcode += '</ul>';
-        cont.innerHTML = hcode;
-    }else if(btxt == '뒷배열추가요~!'){
-        fruit.push(sel.value);
-    }else if(btxt == '앞배열추가요~!'){
-        fruit.unshift(sel.value);
-    }else if(btxt == '뒷배열삭제요~!'){
-        fruit.pop();
-    }else if(btxt == '앞배열삭제요~!'){
-        fruit.shift();
-    }else if(btxt == '중간배열삭제'){
-        fruit.splice(anum.value, delnum.value);
-    }else if(btxt == '중간배열삽입'){
-        fruit.splice(anum.value, 0, sel.value);
+    let S = today.getSeconds();
+    tt[7].innerText = addZero(S);
+}
+
+let rdn = Math.random();
+const rimg = ["https://img.etnews.com/photonews/2110/1461216_20211007085904_466_0001.jpg", "https://d2qqqnyszmt41w.cloudfront.net/wp-content/uploads/2021/04/23150534/202104231445162082.jpg", "https://img.imbc.com/adams/Program/202111/132804027350463581.jpg", "https://image.ytn.co.kr/general/jpg/2021/0925/202109251350012465_d.jpg"];
+
+const imbx = dFn.qs('.imbx');
+
+rimg.forEach(val => {
+    imbx.innerHTML += `
+        <div>
+            <img src = '${val}'>
+        </div>
+    `;
+});
+
+const target = dFn.qsa('.imbox div');
+
+setInterval(colorImg, 2000);
+
+let bNum = 1000;
+function colorImg(){
+    let rbmNum = Math.floor(Math.random()*4);
+    while(rbmNum == bNum){
+        rbmNum = Math.floor(Math.random()*4);
     }
+    bNum = rbmNum;
 
-    showArray();
-    bindCombo();
+    target.forEach((ele, idx)=>{
+        if(idx == rbmNum) ele.classList.add('on');
+        else ele.classList.remove('on');
+    })
 }
