88c7c58c8b95193b645f7a978fac45c273818543
ogreencakeo <magente2000@gmail.com>
2023-09-27 12:08:55 +0900

    Update main.js

--- 003.JS학습/04.JS게임/js/main.js
+++ 003.JS학습/04.JS게임/js/main.js
@@ -67,11 +67,15 @@ function goGame(){
     // 2. 버튼별 기능 분기하기
     if(btxt == '토끼출발'){
         goR1();
-    }
-    if(btxt == '거북출발'){
+    }else if(btxt == '거북출발'){
         // 거북위치값 이동셋팅
         t1pos += 16;
         t1.style.left = t1pos + 'px'; 
+        // 토끼출발 호출
+        goR1();
+    }else if(btxt == '처음으로'){
+        // 페이지 리로드하기
+        location.reload();
     }
 }
 
