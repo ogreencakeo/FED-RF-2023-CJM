cb7b99d677d43b5979e01b85caa6dc5d533543f0
jimin0427 <cjm0427@hs.ac.kr>
2023-09-29 20:00:07 +0900

    e

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/pra.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/pra.js
@@ -1,6 +1,6 @@
 import dFn from "./dom.js";
 import { startSS, setPos } from "./smoothScroll23.js";
-import { gridData, gnbData, previewData } from "./data_drama.js";
+import { gridData, gnbData, previewData, clipData } from "./data_drama.js";
 
 startSS();
 
@@ -12,55 +12,48 @@ setTimeout(() => {
 dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
 dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
 
-startSS();
-
 let desc_box = document.querySelectorAll(".desc-box");
 
 desc_box.forEach((ele) => {
+
     ele.onwheel = (e) => e.stopPropagation();
 });
 
-const grid_box = dFn.qsa('.grid-box');
-grid_box.forEach((ele,idx) => {
-    makeGrid(ele, idx);
-});
+const gird_box = dFn.qsa('.grid-box');
+gird_box.forEach((ele, idx) => makeGrid(ele, idx));
 
 function makeGrid(ele, idx){
     let hcode = '<ul>';
-    gridData[idx].forEach(val => {
+    gridData[idx].forEach((val) => {
         hcode += `
             <li>
                 <figure>
-                <img src="images/${idx ? "poster_img" : "live_photo"}/${val.imgName}.jpg" alt="${val.title}">
+                    <img src = 'images/${idx? "poster_img" : "live_photo"}/${val.imgName}.jpg' alt=${val.imgName}/>
                     <figcaption>${val.title}</figcaption>
                 </figure>
             </li>
         `;
-        hcode += '</ul>'
     });
+    hcode += '</ul>';
     ele.innerHTML = hcode;
 }
 
-const gnbList = dFn.qsa('.gnb li');
-
+const gnbList = dFn.qsa('.gnb ul li');
 gnbList.forEach(ele => {
     let atxt = dFn.qsEl(ele, 'a').innerText;
-    let gData = gnbData[atxt];
-    if(gData){
-        ele.innerHTML = `
+    let gnb_data = gnbData[atxt];
+    if(gnb_data){
+        ele.innerHTML += `
             <div class = 'smenu'>
                 <aside class = 'smbx'>
-                    <h2>${atxt}</h2>
                     <ol>
-                        ${gData
-                            .map(
-                                (val) => `
-                            <li>
-                                <a href="#">${val}</a>
-                            </li>
-                        `
-                            )
-                            .join("")}
+                        ${gnb_data.map((val)=>
+                            `
+                                <li>
+                                    <a href="#">${val}</a>
+                                </li>
+                            `
+                        ).join("")}
                     </ol>
                 </aside>
             </div>
@@ -68,11 +61,12 @@ gnbList.forEach(ele => {
     }
 });
 
-const gnb = dFn.qsa('.gnb li');
+const gnb = dFn.qsa('.gnb ul li');
 gnb.forEach(ele => {
-    if(dFn.qsEl(ele, '.smenu'))
+    if(dFn.qsEl(ele, '.smenu')){
         dFn.addEvt(ele, 'mouseover', overFn);
         dFn.addEvt(ele, 'mouseout', outFn);
+    }
 });
 
 function overFn(){
@@ -81,6 +75,13 @@ function overFn(){
 }
 
 function outFn(){
-    dFn.qsEl(this, '.smenu').style.height = '0';
+    dFn.qsEl(this, '.smenu').style.height = 0 + 'px';
 }
 
+const mvBx = dFn.qs('.intro-mv-img');
+
+dFn.addEvt(mvBx, 'click', showMv);
+
+function showMv(){
+    
+}
