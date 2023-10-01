17b0e53879d68a360e00802901802bbb6e3e6665
jimin0427 <cjm0427@hs.ac.kr>
2023-10-01 15:57:55 +0900

    w

---
+++ 003.JS학습/js/pra.js
@@ -0,0 +1,73 @@
+import dFn from './dom.js';
+
+const fruit = ["배", "사과", "용과", "딸기"];
+
+const frObj = {
+    배: "fruits_01",
+    사과: "fruits_02",
+    용과: "fruits_14",
+    딸기: "fruits_09",
+    두리안: "fruits_17",
+    바나나: "fruits_15",
+    수박: "fruits_12",
+    파인애플: "fruits_13",
+    망고: "fruits_24",
+    오렌지: "fruits_03",
+    체리: "fruits_05",
+    멜론: "fruits_11",
+    블루베리: "fruits_20",
+    레몬: "fruits_04",
+};
+
+const mbtn = dFn.qsa('.mbtn');
+const showit = dFn.qs('.showit');
+const cont = dFn.qs('.cont');
+const sel = dFn.qs('#sel');
+const anum = dFn.qs('#anum');
+const delNum = dFn.qs('#delnum');
+
+const showArray = () => showit.innerHTML = fruit.join('/');
+showArray();
+const bindCombo = () => { anum.innerHTML = 
+    fruit.map((ele, idx) => 
+        `
+            <option value="${idx}">${ele}</option>
+        `
+    ).join("")};
+bindCombo();
+
+mbtn.forEach(ele => dFn.addEvt(ele, 'click', showFruit));
+
+sel.innerHTML = Object.keys(frObj).map((val) => `<option>${val}</option>`).join("");
+
+function showFruit(){
+    const btxt = this.innerText;
+    // console.log('btxt :', btxt);
+
+    if(btxt == '과일주세요~!'){
+        let hcode = '';
+        fruit.forEach(ele => {
+            hcode += `
+                <li style = 'background: url(./addimg/${frObj[ele]}.png) no-repeat center/cover'/>
+                    ${ele}
+                </li>
+            `
+        });
+        cont.innerHTML = `<ul>${hcode}</ul>`;
+    }else if(btxt == '뒷배열추가요~!'){
+        fruit.push(sel.value);
+    }else if(btxt == '앞배열추가요~!'){
+        fruit.unshift(sel.value);
+    }else if(btxt == '뒷배열삭제요~!'){
+        fruit.pop();
+    }else if(btxt == '앞배열삭제요~!'){
+        fruit.shift();
+    }else if(btxt == '중간배열삭제'){
+        fruit.splice(anum.value, delNum.value);
+    }else if(btxt == '중간배열삽입'){
+        fruit.splice(anum.value, 0, sel.value);
+    }
+
+    showArray();
+    bindCombo();
+}
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/pra.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/pra.js
@@ -12,53 +12,45 @@ setTimeout(() => {
 dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
 dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
 
-const gridBox = dFn.qsa('.grid-box');
-gridBox.forEach((ele, idx) => makeGrid(ele, idx));
+const gridBx = dFn.qsa('.grid-box');
+gridBx.forEach((ele, idx) => makeGrid(ele, idx));
 
 function makeGrid(ele, idx){
-    let hcode = '';
-    let grid_data = gridData[idx];
-    console.log('grid_data :', grid_data);
-    grid_data.map((val) => 
-        hcode +=  `
+    let hcode ='';
+    gridData[idx].forEach(val => {
+        hcode += `
             <li>
                 <figure>
-                    <img src = 'images/${idx? 'poster_img':'live_photo'}/${val.imgName}.jpg' alt = '${val.title}'/>
+                    <img src = 'images/${idx? 'poster_img' : 'live_photo'}/${val.imgName}.jpg' alt='${val.title}'/>
                     <figcaption>${val.title}</figcaption>
                 </figure>
             </li>
-        `
-    );
+        `;
+    });
     ele.innerHTML = `<ul>${hcode}</ul>`;
 }
 
 const gnbList = dFn.qsa('.gnb ul li');
+const gnb = dFn.qsa('.gnb ul li');
+
 gnbList.forEach(ele => {
     let atxt = dFn.qsEl(ele, 'a').innerText;
     let gnb_data = gnbData[atxt];
-
     if(gnb_data){
         ele.innerHTML += `
-            <div class='smenu'>
+            <div class = 'smenu'>
                 <aside class='smbx'>
-                    <h2>${atxt}</h2>
+                    <h3>${atxt}</h3>
                     <ol>
-                        ${gnb_data.map((val)=> 
-                            `
-                                <li>
-                                    <a href = '#'>${val}</a>
-                                </li>
-                            
-                            `
-                            ).join("")}
+                        ${gnb_data.map(val => 
+                                `<li>${val}</li>`
+                        ).join("")}
                     </ol>
                 </aside>
             </div>
         `;
     }
 });
-
-const gnb = dFn.qsa('.gnb ul li');
 
 gnb.forEach(ele => {
     if(dFn.qsEl(ele, '.smenu')){
@@ -76,15 +68,15 @@ function outFn(){
     dFn.qsEl(this, '.smenu').style.height = 0 + 'px';
 }
 
-const preBox = dFn.qsa('.preview-box div');
-let preNewData = previewData.sort((x, y)=>{
+const preBx = dFn.qsa('.preview-box div');
+let preNewData = previewData.sort((x, y) => {
     let a = Number(x.idx);
     let b = Number(y.idx);
 
     return (a==b? 0:a>b? -1:1);
 });
 
-preBox.forEach((ele, idx) => {
+preBx.forEach((ele, idx) => {
     let pre_data = preNewData[idx];
     ele.innerHTML += `
         <div>
@@ -93,52 +85,49 @@ preBox.forEach((ele, idx) => {
         </div>
     `;
 });
-
-const clipBox = dFn.qs('.clip-box');
-let hcode = '';
-clipData.forEach((ele)=>{
-    hcode += `
-    <li>
-        <div class = 'clip-mv-box'>
-            <img src = './images/clip_img/${ele.idx}.jpg' alt='${ele.title}'/>
-        </div>
-        <h4>${ele.subtit}</h4>
-        <h3>${ele.title}</h3>
-    </li>
+const clipBx = dFn.qs('.clip-box');
+let clipCode = '';
+clipData.forEach(ele => {
+    clipCode += `
+        <li>
+            <div class='clip-mv-box'>
+                <img src = './images/clip_img/${ele.idx}.jpg' alt = '${ele.title}'/>
+            </div>
+            <h4>${ele.subtit}</h4>
+            <h3>${ele.title}</h3>
+        </li>
     `;
 });
-clipBox.innerHTML = `<ul>${hcode}</ul>`;
 
-const btnBox = dFn.qsa('.btn-box button');
+clipBx.innerHTML = `<ul>${clipCode}</ul>`;
 
-const clipList =dFn.qs('.clip-box ul');
-
-const  CNT_LIST = dFn.qsEl(clipList, 'li').length;
-const LIMIT_LIST = 4;
-const LIMIT_MOVE = CNT_LIST- LIMIT_LIST;
+const btnBx = dFn.qsa('.btn-box button');
+const clipList = dFn.qs('.clip-box ul');
+const LIST_LENGTH = dFn.qsaEl(clipList, 'li').length;
+const LIMT_LIST = 4;
+const LIMIT_MOVE = LIST_LENGTH-LIMT_LIST;
 const BLOCK_NUM = 25.5;
 let mvNum = 0;
 
-btnBox.forEach(ele => dFn.addEvt(ele, 'click', moveClip));
+btnBx.forEach(ele => dFn.addEvt(ele, 'click', moveClip));
 
 function moveClip(){
     let isR = this.classList.contains('fa-chevron-right');
-
     if(isR){
         mvNum++;
-        if(mvNum > LIMIT_MOVE){
+        if(mvNum>LIMIT_MOVE){
             mvNum = LIMIT_MOVE;
-            btnBox[1].style.display = 'none';
+            btnBx[1].style.display = 'none';
         }else{
-            btnBox[0].style.display = 'block';
+            btnBx[0].style.display = 'block';
         }
     }else{
         mvNum--;
         if(mvNum<0){
             mvNum = 0;
-            btnBox[0].style.display = 'none';
+            btnBx[0].style.display = 'none';
         }else{
-            btnBox[1].style.display = 'block';
+            btnBx[1].style.display = 'block';
         }
     }
     clipList.style.left = -(BLOCK_NUM * mvNum) + '%';
