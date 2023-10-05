2413e335be4d2cfdd6afbee30985881b080686e4
ogreencakeo <magente2000@gmail.com>
2023-10-05 16:09:40 +0900

    Update js4-4.array_sort.js

--- 003.JS학습/js/js4-4.array_sort.js
+++ 003.JS학습/js/js4-4.array_sort.js
@@ -1,7 +1,7 @@
 // JS4-4. 배열의 정렬과 검색 JS
 
 // DOM메서드
-import dFn from './dom.js';
+import dFn from "./dom.js";
 
 // console.log(dFn);
 
@@ -43,6 +43,47 @@ import dFn from './dom.js';
     [-> 기준정렬 : 내림차순]
     배열변수.reverse() -> 기본정렬
 
+    //////////////////////////////////////////////////
+    ->>> 숫자형, 문자형에 무관하게 하나로 처리하기
+    [ sort() 메서드만 사용하여 정렬잡기 ]
+    (비교함수사용)
+    배열변수.sort(function(x,y){
+        if(x>y) return 1;
+        if(x<y) return -1;
+        return 0;
+    })
+    
+    -> 단순화하기(삼항연산자사용!)
+
+    배열변수.sort(function(x,y){
+        return x == y ? 0 : x > y ? 1 : -1;
+    })
+
+    -> 리턴값의 의미(오름차순)
+    1) if(x>y) return 1; -> x가 y뒤에 옴 (리턴값 양수)
+    2) if(x<y) return -1; -> x가 y앞에 옴 (리턴값 음수)
+    3) return 0; -> x,y 정렬을 유지 (리턴값 0)
+
+    -> 내림차순은 양수/음수만 반대로 써주면 된다!
+
+    [ 정렬시 데이터 유의사항 ]
+    1. 문자형일 경우 대소문자가 섞여있으면 대문자나 소문자중
+    하나로 통일하여 비교해야함(toUpperCase()/toLowerCase())
+    예)
+    배열변수.sort((x,y)=>{
+        let a = x.toUpperCase(),
+            b = y.toUpperCase();
+        
+        return a == b ? 0 : a > b ? 1 : -1;
+    })
+
+    2. 날짜정렬도 숫자와 동일함(날짜데이터 자차가 숫자형으로 되어있음)
+
+    3. 특정언어의 특수문자일 경우 localeCompare() 메서드로 
+    문자열 비교를 한다!
+    예) 특수문자 x변수를 y변수와 변환후 비교 
+    x.localeCompare(y)
+
 
 ******************************************************/
 
@@ -67,13 +108,14 @@ const arrString = ["파", "타", "하", "가", "바", "사", "다", "라", "차"
 // map()메서드로 배열값을 태그로 감싸서 출력하기
 
 // (1) 출력대상: .showNum
-const showNum = dFn.qs('.showNum');
+const showNum = dFn.qs(".showNum");
 
 // (2) 배열 숫자 데이터 만큼 이미지로 변환하여 출력
 // map().join() 맵쬬잉!
 const showScreen = () =>
-showNum.innerHTML = arrNumber.map(val=>
-    `<img src="./images/num/num_0${val}.png" alt="숫자${val}이미지">`).join('');
+    (showNum.innerHTML = arrNumber
+        .map((val) => `<img src="./images/num/num_0${val}.png" alt="숫자${val}이미지">`)
+        .join(""));
 
 // 최초출력
 showScreen();
@@ -81,28 +123,29 @@ showScreen();
 // (3) 정렬 기준에 선택박스 변경 이벤트 발생시
 // 정렬 변경하기 (오름차순/내림차순)
 // (3-1) 대상: #sel
-const selBox = dFn.qs('#sel');
+const selBox = dFn.qs("#sel");
 // (3-2) 이벤트 연결하기 : 이벤트 종류 - change
-dFn.addEvt(selBox,'change',function(){
+dFn.addEvt(selBox, "change", function () {
     // 1. 선택 option value값
     let optVal = this.value;
-    console.log('숫자정렬변경:',optVal);
+    console.log("숫자정렬변경:", optVal);
     // 2. 정렬 분기 : 1 - 오름차순 / 2 - 내림차순
-    if(optVal==1){ // 오름차순
+    if (optVal == 1) {
+        // 오름차순
         // sort() 빼기연산처리 : 앞수-뒷수
         // arrNumber.sort((a,b)=>a-b);
 
-        arrNumber.sort((a, b) => a==b? 0:a>b? 1:-1);
-    } 
-    else if(optVal==2){ // 내림차순
+        arrNumber.sort((a, b) => (a == b ? 0 : a > b ? 1 : -1));
+    } else if (optVal == 2) {
+        // 내림차순
         // sort() 빼기연산처리 : 뒷수-앞수
         // arrNumber.sort((a,b)=>b-a);
 
-        arrNumber.sort((a, b) => a==b? 0:a>b? -1:1);
+        arrNumber.sort((a, b) => (a == b ? 0 : a > b ? -1 : 1));
     } ///// else if ////
 
-    // 화면출력 
-    // -> 원본 배열의 정렬이 변경된 후 다시 출력 
+    // 화면출력
+    // -> 원본 배열의 정렬이 변경된 후 다시 출력
     showScreen();
 }); ///// change 이벤트 함수 //////////
 
@@ -111,13 +154,11 @@ dFn.addEvt(selBox,'change',function(){
 // map()메서드로 배열값을 태그로 감싸서 출력하기
 
 // (1) 출력대상: .showNum
-const showNum2 = dFn.qs('.showNum2');
+const showNum2 = dFn.qs(".showNum2");
 
 // (2) 배열 숫자 데이터 만큼 이미지로 변환하여 출력
 // map().join() 맵쬬잉!
-const showScreen2 = () =>
-showNum2.innerHTML = arrString.map(val=>
-    `<span>${val}</span>`).join('');
+const showScreen2 = () => (showNum2.innerHTML = arrString.map((val) => `<span>${val}</span>`).join(""));
 
 // 최초출력
 showScreen2();
@@ -125,31 +166,30 @@ showScreen2();
 // (3) 정렬 기준에 선택박스 변경 이벤트 발생시
 // 정렬 변경하기 (오름차순/내림차순)
 // (3-1) 대상: #sel
-const selBox2 = dFn.qs('#sel2');
+const selBox2 = dFn.qs("#sel2");
 // (3-2) 이벤트 연결하기 : 이벤트 종류 - change
-dFn.addEvt(selBox2,'change',function(){
+dFn.addEvt(selBox2, "change", function () {
     // 1. 선택 option value값
     let optVal = this.value;
-    console.log('숫자정렬변경:',optVal);
+    console.log("숫자정렬변경:", optVal);
     // 2. 정렬 분기 : 1 - 오름차순 / 2 - 내림차순
-    if(optVal==1){ 
+    if (optVal == 1) {
         // sort() 메서드 내부함수 사용법
         // [ a > b 일때 true이면 1처리 -> 순서바꿈! ]
-        arrString.sort((a, b) => a==b? 0:a>b? 1:-1);
+        arrString.sort((a, b) => (a == b ? 0 : a > b ? 1 : -1));
 
         // 오름차순 기본 메서드 sort() 사용
         // arrString.sort();
-    } 
-    else if(optVal==2){ 
-        // [ sort() 메서드 내부함수 사용법 ]   
+    } else if (optVal == 2) {
+        // [ sort() 메서드 내부함수 사용법 ]
         // a > b 일때 true이면 1처리 -> 순서안바꿈!
-        arrString.sort((a, b) => a==b? 0:a>b? -1:1);
+        arrString.sort((a, b) => (a == b ? 0 : a > b ? -1 : 1));
 
         // 내림차순 기본 메서드 reverse() 사용
         // arrString.reverse();
-    } 
+    }
 
-    // 화면출력 
-    // -> 원본 배열의 정렬이 변경된 후 다시 출력 
+    // 화면출력
+    // -> 원본 배열의 정렬이 변경된 후 다시 출력
     showScreen2();
-}); 
+});
