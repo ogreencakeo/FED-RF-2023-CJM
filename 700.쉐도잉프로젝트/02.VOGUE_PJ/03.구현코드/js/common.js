2e5377eaa6bd4e6484605398347858db540957b4
ogreencakeo <magente2000@gmail.com>
2023-10-10 17:01:21 +0900

    dsf

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
@@ -1,1 +1,16 @@
 // 보그PJ - 공통 모듈 JS : common.js
+
+// DOM 메서드
+import dFn from './dom.js'
+
+// 상단, 하단 공통 데이터 불러오기
+import tData from './data/com_module.js';
+
+// 대상성정 : .common-area
+const comArea = dFn.qsa('.common-area');
+console.log('tData :', tData, ', comArea :', comArea);
+
+// 상단영역 html 넣기
+comArea[0].innerHTML = tData.topArea;
+// 하단영역 html 넣기
+comArea[1].innerHTML = tData.footerArea;
