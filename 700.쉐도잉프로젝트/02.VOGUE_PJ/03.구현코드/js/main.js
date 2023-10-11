d97f7f5c2b94d71e7476fbe75963dc46d654430f
ogreencakeo <magente2000@gmail.com>
2023-10-11 17:22:00 +0900

    fsd

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -0,0 +1,14 @@
+// 보그 PJ 메인 페이지 js - main.js
+
+import dFn from './dom.js';
+
+// [ 1. 메인 페이지 등장 액션 클래스 넣기 ]
+// 대상 : .main-area section
+const hideBox = dFn.qsa('.main-area section');
+
+// 첫번째 박스 빼고 숨김 클래스 넣기
+hideBox.forEach((ele, idx) => {
+    if(idx != 0){
+        ele.classList.add('scAct');
+    }
+});
