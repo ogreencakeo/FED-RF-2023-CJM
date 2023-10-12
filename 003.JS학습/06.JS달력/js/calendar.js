d5b728ef4e61cc71a50d3ce72132165a33562ebd
ogreencakeo <magente2000@gmail.com>
2023-10-12 15:50:29 +0900

    fds

--- 003.JS학습/06.JS달력/js/calendar.js
+++ 003.JS학습/06.JS달력/js/calendar.js
@@ -5,7 +5,17 @@ const Dfn = {
     qs : x => document.querySelector(x),
     qsa : x => document.querySelectorAll(x),
     cg : x => console.log(x),
+    // 날짜 찍기 형식 변경 함수(yyyy-mm-dd시간)
+    fm : x => `
+        ${x.getFullYear()
+        }-${x.getMonth()+1 <10? '0'+(x.getMonth()+1) : x.getMonth()+1
+        }-${x.getDate() <10? '0'+x.getDate() : x.getDate()
+        } (${week[x.getDay()]})
+    `
 };  // dFn 객체
+
+// 요일변경배열
+const week = ['일', '월', '화', '수', '목', '금', '토']; 
 
 // 달력함수 호출
 makeDallyeok();
@@ -32,12 +42,36 @@ function makeDallyeok(){
 
     //////////////////////////////////////////////////////////////
     // 2. 함수 만들기
-    // (1) 달력 초기화구성 함수
+    // 2-1. 달력 초기화구성 함수
     const initDallyeok = () => {
-        // 1. 전달 마지막 날짜(옵션:0) 
+        // 현재년
+        let cYr = currDate.getFullYear();
+        // 현재달
+        let cMt = currDate.getMonth();
+
+        // [ 선택달 끝날짜, 첫날짜 알아오기 ]
+        // new Date(년도, 월, 옵션)
+        // 전달값
+        // (1) 년도
+        // (2) 월
+        // (3) 옵션 : 0 - 현재달끝날짜 / 1 - 다음달첫날짜
+
+        // 2-1-1. 전달 마지막 날짜(옵션:0) 
         // -> 달력처음 전달 끝쪽 날짜 표시
+        const prevLast = new Date(cYr, cMt, 0);
+        Dfn.cg('전달 끝날짜 :' + Dfn.fm(prevLast));
+
+        // 2-1-2. 현재달 첫째날짜(옵션:1)
+        // -> 전달로 셋팅
+        //  달력 전달 셋팅을 위한 요일 구하기
+        const thisFirst = new Date(cYr, cMt, 1);
+        Dfn.cg('현재달 첫째날짜 :' + Dfn.fm(thisFirst));
+
 
 
     };  // initDallyeok함수 ///////////////
+
+    // 초기셋팅함수 호출
+    initDallyeok();
 
 }   // makeDallyeok함수 //////////////////////////////
