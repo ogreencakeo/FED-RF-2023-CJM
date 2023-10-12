0f51eb5b2253fb84f89eb3acba85a4ea1a83a1c1
ogreencakeo <magente2000@gmail.com>
2023-10-12 10:55:02 +0900

    sfd

--- 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -24,13 +24,13 @@
 /////////////////////////////////////////////////////////////////////////////
 // [ 0. 주인공들 변수에 할당! ]
 // 0-1. 미니언즈
-const mi = $('.mi');
+const mi = $(".mi");
 // 0-2. 건물 li
-const room = $('.building li');
-// 0-3. 버튼들 
-const btns = $('.btns button');
+const room = $(".building li");
+// 0-3. 버튼들
+const btns = $(".btns button");
 // 0-4. 메시지 박스
-const msg = $('.msg');
+const msg = $(".msg");
 // 0-5. 좀비, 주사기 요소 변수처리
 let mz1 = `<img src="./images/mz1.png" alt="좀비1" class="mz">`;
 let mz2 = `<img src="./images/mz2.png" alt="좀비2" class="mz">`;
@@ -41,38 +41,38 @@ let inj = `<img src="./images/inj.png" alt="주사기" class="inj">`;
 ///////////////////////////////////////////////////////////////////////////////
 // [ 1. 건물 각 방에 번호넣기 + 좀비/주사기 넣기 ]
 // 대상 : .building li -> room변수
-// 사용 제이쿼리 메서드 : 
+// 사용 제이쿼리 메서드 :
 // (1) each((순번, 요소) => {}) : 요소의 개수만큼 순서대로 돌아줌
 // (2) append(요소) : 선택요소 내부에 자식요소 추가(이동)
-room.each((idx, ele)=> {
+room.each((idx, ele) => {
     // console.log('idx :', idx, ', ele :', ele);
 
     // 1-1. 각 방에 숫자로 순번 넣기
     $(ele).text(idx);
 
     // 1-2. 좀비/주사기 넣기
-    switch(idx){
-        case 9 :
+    switch (idx) {
+        case 9:
             $(ele).append(mz1);
             break;
-        case 7 :
+        case 7:
             $(ele).append(mz2);
             break;
-        case 2 :
+        case 2:
             $(ele).append(inj);
             break;
-        case 1 :
+        case 1:
             $(ele).append(zom);
             break;
     }
 });
 
 // 좀비는 모두 숨기기
-$('.mz').hide();
+$(".mz").hide();
 // 시간이 없는 hide()는 display:none 처리함
 
 ////////////////////////////////////////////////////////////////////////
-// [ 2. 버튼 셋팅하기 ] 
+// [ 2. 버튼 셋팅하기 ]
 // 대상 : .btns button -> btns변수
 // 2.1 버튼들.숨겨().첫번째().보여()
 btns.hide().first().show();
@@ -83,40 +83,61 @@ btns.hide().first().show();
 // 3-2. seq : 이동할 li방 순번
 // 3-3. fn : 이동후 실행할 코드 (콜백함수)
 const actMini = (ele, seq, fn) => {
+    // 4-0. 메시지 숨기기 + 버튼 숨기기
+    msg.fadeOut(300);
+    // this는 클릭된 버튼자신! -> ele로 전달!
+    $(ele).slideUp(400);
 
+    // 4-1. 위치값 읽기 : seq에 방번호를 보냄
+    // 원리 : 이동할 li방 위치값을 읽은 후 이동함
+    let myRoom = room.eq(seq);
+    // 위치값 배열 변수
+    let pos = [];
+    // top 위치값
+    pos[0] = myRoom.offset().top;
+    // left 위치값 : 방에서 중앙에 위치하도록 보정
+    //  -> left값 + 방 width절반 - 미니언즈 width 절반
+    pos[1] = myRoom.offset().left + myRoom.width() / 2 - mi.width() / 2;
+
+    // 제이쿼리 위치값 정보 메서드 : offset() -> 하위속성 : offset().top, offset().left
+    // 제이쿼리로 가로, 세로 크기정보 메서드 : 
+    // -> 가로크기 width() / 세로크기 height()
+
+    console.log("pos[0] :", pos[0], "/ pos[1] :", pos[1]);
+
+    // 4-2. 이동하기
+    // 대상 : .mi => mi변수
+    // animate({CSS설정}, 시간, 이징, 콜백함수)
+    mi.animate(
+        {
+            top: pos[0] + "px",
+            left: pos[1] + "px",
+        },
+        800,
+        "easeOutElastic",
+        // 콜백함수
+        // function(){ // this가 mi임
+        fn()
+    );
 };
 
 //////////////////////////////////////////////////////////////////////
 // [ 4. "들어가기" 버튼 클릭시 ]
 btns.first() // 첫번째 버튼
-    .click(function(){
-
-        // 4-0. 메시지 숨기기
-        msg.fadeOut(300);
-
-        // 4-1. 위치값 읽기
-        // 원리 : 이동할 li방 위치값을 읽은 후 이동함
-        let myRoom = room.eq(8);
-        // 위치값 배열 변수
-        let pos = [];
-        // top 위치값
-        pos[0] = myRoom.offset().top;
-        // left 위치값
-        pos[1] = myRoom.offset().left;
-        console.log('pos[0] :', pos[0], '/ pos[1] :', pos[1]);
-
-        // 4-2. 이동하기
-        // 대상 : .mi => mi변수
-        // animate({CSS설정}, 시간, 이징, 콜백함수)
-        mi.animate({
-            top : pos[0] + 'px',
-            left : pos[1] + 'px'
-        }, 800, 'easeOutElastic', ()=>{
-            // 메시지 변경 + 메시지 보이기
-            msg.html(`와~! 아늑하다!<br /> 옆방으로 가보자!`)
-            .delay(1000).fadeIn(300);
-        });
+    .click(
+        // () => {
+        function () {   // 하위이벤트함수 this의미
+            // 버튼별 콜백함수 만들기
+            let fn = () => {    // this가 싸고 있는 버튼요소임
+                // 메시지 변경 + 메시지 보이기
+                msg.html(`와~! 아늑하다!<br /> 옆방으로 가보자!`).delay(1000).fadeIn(300);
     
-    });
+                console.log("미니언즈 콜백함수 this :", this);
+                // 다음 버튼 보이기
+                $(this).next().delay(1000).slideDown(400);
+            };  ///// 콜백함수 //////////
 
-
+            // 미니언즈 공통함수 호출
+            actMini(this, 8, fn);
+        }
+    );
