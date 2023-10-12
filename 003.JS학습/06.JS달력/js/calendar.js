e8c7a41e81efa0974e9f64a1a38d6cef45e1c91f
ogreencakeo <magente2000@gmail.com>
2023-10-12 16:20:22 +0900

    Update calendar.js

--- 003.JS학습/06.JS달력/js/calendar.js
+++ 003.JS학습/06.JS달력/js/calendar.js
@@ -35,6 +35,8 @@ function makeDallyeok(){
     const monthTit = Dfn.qs('.monthTit');
     // 1-5. 일요소 : .yearTit
     const dates = Dfn.qs('.dates');
+    // 1-6. 날짜 넣을 배열 변수
+    const dateSet = [];
 
     // Dfn.cg(yearTit);
     // Dfn.cg(monthTit);
@@ -59,15 +61,65 @@ function makeDallyeok(){
         // 2-1-1. 전달 마지막 날짜(옵션:0) 
         // -> 달력처음 전달 끝쪽 날짜 표시
         const prevLast = new Date(cYr, cMt, 0);
-        Dfn.cg('전달 끝날짜 :' + Dfn.fm(prevLast));
+        Dfn.cg('전달 끝날짜 :' + Dfn.fm(prevLast)); // 
 
         // 2-1-2. 현재달 첫째날짜(옵션:1)
         // -> 전달로 셋팅
         //  달력 전달 셋팅을 위한 요일 구하기
         const thisFirst = new Date(cYr, cMt, 1);
-        Dfn.cg('현재달 첫째날짜 :' + Dfn.fm(thisFirst));
+        Dfn.cg('현재달 첫째날짜 :' + Dfn.fm(thisFirst)); //
 
+        // 2-1-3. 현재달 마지막 날짜(옵션:0)
+        const thisLast = new Date(cYr, cMt+1, 0);
+        Dfn.cg('현재달 마지막날짜 : ' + Dfn.fm(thisLast)); // 2023-11-30(목)
 
+        // 2-1-4. 년도 표사하기
+        yearTit.innerHTML = cYr;
+        
+        // 2-1-4. 월 표사하기
+        monthTit.innerHTML = cMt + 2;
+
+        // 2-2. 날짜 데이터 태그 구성하기
+        // 2-2-1. 전달 날짜 앞쪽 채우기
+        // 조건 : 현재달 첫날짜 요일이 0이 아니면 내용있음
+        // -> 왜 0인가? 0은 일요일 이므로 0이면 앞에 내용없음
+        let fDay = thisFirst.getDay;
+        Dfn.cg('이번달 첫날 요일 : ' + fDay); // 3 - 수요일
+        if(this.fDay !=0 ){
+            // 만약 요일 번호가 0이 아니면 for문 돌림
+            for(let i=0; i < fDay; i++){
+                // 반복횟수만큼 배열 앞쪽에 추가
+                // 앞에 추가 메서드 : unshift()
+                dateSet.unshift(`
+                    <span style="color:#ccc" class="bm">
+                        ${prevLast.getDate() - i}
+                    </span>
+                `);
+                // 전달 마지막 날짜 - for문 i값(0, 1, 2, .........)
+            }
+        }
+
+        Dfn.cg('날짜배열 : '+ dateSet);
+
+        // 2-2-2. 현재월 삽입하기
+        // 반복문 구성 : 현재월 1일부터 마지막 날짜까지 반복 배열 추가
+        for(let i=1; i<=thisLast.getDate(); i++){
+            dateSet.push(i);
+        }
+
+        Dfn.cg('날짜배열 : '+ dateSet);
+
+        // 2-2-3. 다음달 나머지 칸 삽입하기
+        // 다음달은 클래스 'am'으로 구분
+        // 반복구성 : 1부터 2주분량 정도 넣는다
+        for(let i=1; i<=14; i++){
+            dateSet.push(`
+                <span style="color:#ccc" class="am">
+                    ${i}
+                </span>
+            `);
+        }  
+        Dfn.cg('날짜배열 : '+ dateSet);
 
     };  // initDallyeok함수 ///////////////
 
