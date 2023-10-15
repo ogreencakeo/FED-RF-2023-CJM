035157beadbe5048ca9297de7231bbb96606d3e3
jimin0427 <cjm0427@hs.ac.kr>
2023-10-15 21:48:59 +0900

    s

--- 003.JS학습/06.JS달력/js/pra.js
+++ 003.JS학습/06.JS달력/js/pra.js
@@ -14,6 +14,7 @@ const Dfn = {
 };
 
 const week = ['일', '월', '화', '수', '목', '금', '토']; 
+
 makeDallyeok();
 function makeDallyeok(){
     const currDate = new Date();
@@ -21,31 +22,95 @@ function makeDallyeok(){
     const yearTit = Dfn.qs('.yearTit');
     const monthTit = Dfn.qs('.monthTit');
     const dates = Dfn.qs('.dates');
-    const dateSet = [];
 
+    const dateSet = [];
     let hcode = '';
 
-    
-    const initDallyeok = () =>{
+    const initDallyeok = () => {
+        dateSet.splice(0);
+        hcode = '';
 
         let cYear = currDate.getFullYear();
         let cMt = currDate.getMonth();
 
-        console.log(cMt);
-
-        const prevLast = new Date(cYear, cMt, 0); 
-        const thisFirst = new Date(cYear, cMt, 1); 
+        const prevLast = new Date(cYear, cMt, 0);
+        const thisFirst = new Date(cYear, cMt, 1);
         const thisLast = new Date(cYear, cMt+1, 0);
 
         yearTit.innerHTML = cYear;
         monthTit.innerHTML = cMt+1;
 
         let fDay = thisFirst.getDay();
-        if(fDay !=0 ){
+        if(fDay != 0){
             for(let i=0; i<fDay; i++){
-                dates
+                dateSet.unshift(`
+                    <span style = "color : #ccc" class="bm">
+                        ${prevLast.getDate()-i}
+                    </span>
+                `);
             }
         }
-    }
+
+        for(let i=1; i<=thisLast.getDate(); i++){
+            dateSet.push(i);
+        }
+
+        for(let i=1; i<=14; i++){
+            dateSet.push(
+                `
+                    <span style="color : #ccc" class="am">
+                        ${i}
+                    </span>
+                `
+            );
+        }
+
+        for(let i=0; i<42; i++){
+            if(
+                (today.getDate()==dateSet[i] && (today.getMonth()==currDate.getMonth()) && today.getFullYear()==currDate.getFullYear())
+            ){
+                hcode += `<div class="date today">${dateSet[i]}</div>`
+            }else{
+                hcode += `<div class="date">${dateSet[i]}</div>`
+            }
+        }
+        dates.innerHTML = hcode;
+
+        let newDate = Dfn.qsa('.date');
+
+        newDate.forEach(ele => {
+            Dfn.addEvt(ele, 'click', ()=>{
+                let nowY = yearTit.innerText;
+                let nowM = monthTit.innerText;
+                let nowD = ele.innerText;
+
+                let isSpan = Dfn.qsEl(ele, 'span');
+                if(isSpan){
+                    let isAM = isSpan.classList.contains('am');
+                    if(isAM){
+                        nowM++;
+                        if(nowM==13){
+                            nowM=1;
+                            nowY++;
+                        }else{
+                            nowM--;
+                            if(nowM==0){
+                                nowM=12;
+                                nowY--;
+                            }
+                        }
+                    }
+                }
+            });
+        }); 
+    };
+    const chgvCalendar = (num) => {
+        currDate.setMonth(currDate.getMonth()+num);
+        initDallyeok();
+    };
+
+    Dfn.addEvt(Dfn.qs('.btnL'), 'click', ()=>chgvCalendar(-1));
+    Dfn.addEvt(Dfn.qs('.btnR'), 'click', ()=>chgvCalendar(1));
+
     initDallyeok();
 }
