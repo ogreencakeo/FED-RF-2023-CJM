012e2213d9325c1e42ac3cc0d6c165ebdad81332
ogreencakeo <magente2000@gmail.com>
2023-10-18 12:20:38 +0900

    j

--- 003.JS학습/06.JS달력/모듈화/js/calendar.js
+++ 003.JS학습/06.JS달력/모듈화/js/calendar.js
@@ -24,7 +24,7 @@ const week = ['일', '월', '화', '수', '목', '금', '토'];
 // 달력함수 호출
 makeDallyeok();
 
-function makeDallyeok(){
+function makeDallyeok(selEl){   // selEl - 달력 넣을 요소
     Dfn.cg('달력만들어!');
 
     //////////////////////////////////////////////////////////////
@@ -228,3 +228,42 @@ function makeDallyeok(){
     initDallyeok();
 
 }   // makeDallyeok함수 //////////////////////////////
+
+//////////////////////////////////////////////////////////////
+// 3. 함수명 : insertHcode
+// 기능 : 달력의 HTML 코드 넣기
+function insertHcode(){
+    // 달력 html 코드를 리턴함
+    return `
+        <!-- 달력전체박스 -->
+        <div class="calender">
+            <!-- 달력상단:해당년/월표시 -->
+            <header class="header">
+                <!-- 달력이동버튼:이전 -->
+                <button class="mbtn btnL">〈</button>
+                <div class="title">
+                    <div class="yearTit"></div>
+                    <div class="monthTit"></div>
+                </div>
+                <!-- 달력이동버튼:다음 -->
+                <button class="mbtn btnR">〉</button>
+            </header>
+            <!-- 달력날짜표시박스 -->
+            <section class="main">
+                <!-- 주단위 구분박스 -->
+                <div class="week">
+                    <div class="day">Sun</div>
+                    <div class="day">Mon</div>
+                    <div class="day">Tue</div>
+                    <div class="day">Wed</div>
+                    <div class="day">Thu</div>
+                    <div class="day">Fri</div>
+                    <div class="day">Sat</div>
+                </div>
+                <!-- 해당월의 달력날짜 구성박스 -->
+                <div class="dates">
+                </div>
+            </section>
+        </div>
+    `;
+}
