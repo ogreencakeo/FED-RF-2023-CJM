db404929ce1503a52a0534265abe88d7151e8dea
jimin0427 <cjm0427@hs.ac.kr>
2023-10-24 20:58:49 +0900

    z

--- 003.JS학습/js/pra.js
+++ 003.JS학습/js/pra.js
@@ -1,41 +1,47 @@
-import dfn from "./dom.js";
+import dfn from './dom.js';
 
 const btnLocal = dfn.qsa('.local-box button');
-btnLocal.forEach(ele => dfn.addEvt(ele, 'click', localFn));
+btnLocal.forEach(ele => dfn.addEvt(ele, 'click', localSFn));
 
 dfn.qsa('.local ul li').forEach((ele, idx)=>{
-    const keyName = ['lname', 'lrole', 'lcat'];
     ele.onclick = function(){
+        const keyName = ['lname', 'lrole', 'lcat'];
         localStorage.removeItem(keyName[idx]);
     }
-});
+})
 
-function localFn(){
+function localSFn(){
     let btxt = this.innerText;
     if(btxt == '처음'){
-        localStorage.removeItem('lname', '이정재');
-        localStorage.removeItem('lrole', '박평호');
-        localStorage.removeItem('lcat', '조직내 스파이를 색출하는 해외팀 안기부 팀장');
+        localStorage.setItem('lname', '이정재');
+        localStorage.setItem('lrole','박평호역');
+        localStorage.setItem('lcat','조직내 스파이를 색출하는 해외팀 안기부팀장');
+    } else if (btxt == '보여줘') {
+        dfn.qs('.local .nm').innerText = localStorage.getItem('lname');
+        dfn.qs('.local .role').innerText = localStorage.getItem('lrole');
+        dfn.qs('.local .cat').innerText = localStorage.getItem('lcat');
     }else if(btxt == '전체삭제'){
         localStorage.clear();
-    }else if(btxt == '보여줘'){
-        dfn.qs('.local .nm').innerText = localStorage.getItem('lname');
-        dfn.qs('.local. .role').innerText = localStorage.getItem('lrole');
-        dfn.qs('.local .cat').innerText = localStorage.getItem('lcat'); 
-    }else if(btxt == '처리'){
-        if(!localStorage.getItem('minfo')) makeObj();
         bindData();
+        bindMod();
+    }else if(btxt == '처리'){
+        if(!localStorage.getItem('minfo') || localStorage.getItem('minfo') == '[]') makeObj();
+        bindData();
+        bindMod();
     }
 }
+
+bindData();
 
 function makeObj(){
     let obj = [
         {
             idx: 1,
-            tit: "내가 왕이 될 상인가?",
-            cont: "이정재형은 진정 왕이십니다.",
+            tit: '내가 왕이될 상인가?',
+            cont: '이정재형은 진정 왕이십니다~!',
         },
     ];
+
     localStorage.setItem('minfo', JSON.stringify(obj));
 }
 
@@ -44,21 +50,23 @@ function bindData(){
     let bindCode = '';
     if(localData){
         localData = JSON.parse(localData);
-        bindCode = localData.localeCompare((v, i) => `
+        bindCode = localData.map((v, i) => `
             <tr>
                 <td>${v.idx}</td>
                 <td>${v.tit}</td>
                 <td>${v.cont}</td>
-                <td class="del-link">
+                <td class='del-link'>
                     <a href="#" data-idx="${i}">X</a>
                 </td>
             </tr>
         `).join('');
     }else{
         bindCode = `
-            <tr colspan = '4'>
-                <td>데이터가 없습니다.</td>
-            </tr>
+        <tr>
+            <td colspan="4" style="text-align:center">
+                데이터가 없습니다...
+            </td>
+        </tr>
         `;
     }
 
@@ -75,133 +83,128 @@ function bindData(){
     `;
 
     dfn.qs('.board').innerHTML = hcode;
-    dfn.qsa('.board .del-link a').forEach(ele =>
-        dfn.addEvt(ele, 'click', ()=> delRec(ele.getAttribute('data-idx'))));
-}
 
-dfn.addEvt(dfn.qs('#sbtn'), 'click', insData);
-
-function insData(){
-
-    let tit = dfn.qs('#tit').value;
-    let cont = dfn.qs('#cont').value;
-
-    if(tit.trim()=='' || cont.trim()==''){
-        alert('입력데이터가 없습니다. 모두 입력하세요');
-        return;
-    }
-
-    let orgData = localStorage.getItem('minfo');
-    if(!orgData){
-        localStorage.setItem('minfo', '[]');
-        orgData = localStorage.getItem('minfo');
-    }
-    orgData = JSON.parse(orgData);
-
-    if(orgData.length != 0){
-        orgData.sort((a, b) => {
-            return (a.idx==b.idx? 0:a.idx>b.idx? 1:-1);
-        });
-    }
-
-    let lastArr = orgData.length == 0?
-        0 : orgData[orgData.length-1].idx;
-    
-    orgData.push({
-        'idx' : lastArr+1,
-        'tit' : tit,
-        'cont' : cont
-    })
-    localStorage.setItem('minfo', JSON.stringify(orgData));
-
-    bindData();
-    bindMode();
-
+    dfn.qsa('.board .del-link a').forEach(ele => dfn.addEvt(ele, 'click', 
+    ()=>delRec(ele.getAttribute('data-idx'))));
 }
 
 function delRec(idx){
     event.preventDefault();
     let orgData = localStorage.getItem('minfo');
+    
     orgData = JSON.parse(orgData);
-
-    if(confirm('정말정말 삭제하시겠습니까?')){
-        orgData.splice(idx, 1);
+    if(confirm('정말정말 지우시게여?')){
+        orgData.splice(idx,1);
+        localStorage.setItem('minfo', JSON.stringify(orgData));
 
         bindData();
-        bindMode();
+        bindMod();
     }
-
 }
 
-const modSel =dfn.qs('#sel');
-const modTit =dfn.qs('#tit2');
-const modCont =dfn.qs('#cont2');
-const modBtn =dfn.qs('#mobtn');
+const modSel = dfn.qs('#sel');
+const modTit = dfn.qs('#tit2');
+const modCont = dfn.qs('#cont2');
+const moBtn = dfn.qs('#mobtn');
 
-function bindMode(){
+function bindMod(){
     let orgData = localStorage.getItem('minfo');
     if(!orgData){
         localStorage.setItem('minfo', '[]');
-        orgData = localStorage.getItem('minfo');
+        orgData = localStorage.getItem('minfo')
     }
-
     orgData = JSON.parse(orgData);
 
-    modSel.innerHTML = `<option value='show'>항목선택</option>`;
+    modSel.innerHTML = `<option value ='show'>항목 선택</option>`;
     orgData.forEach(v=>{
         modSel.innerHTML += `
-            <option value="${v.idx}">${v.tit}</option>
-        `
-    });
+            <option value=${v.idx}>${v.tit}</option>
+        `;
+    })
     
 }
 
-bindMode();
+bindMod();
 
-dfn.addEvt(modSel, 'change', setMode);
+dfn.addEvt(dfn.qs('#sbtn'), 'click', insData);
 
-function setMod(){
-    let optVal = this.value;
-    if(optVal == 'show') return;
+function insData(){
+    let tit = dfn.qs('#tit').value;
+    let cont = dfn.qs('#cont').value;
+
+    if(tit.trim()=='' || cont.trim()==''){
+        alert('입력데이터가 없습니다.');
+        return;
+    }
+
     let orgData = localStorage.getItem('minfo');
     if(!orgData){
         localStorage.setItem('minfo', []);
         orgData = localStorage.getItem('minfo');
     }
+    orgData = JSON.parse(orgData);
 
+    if(orgData.length !=0){
+        orgData.sort((a, b)=> {
+            return a.idx==b.idx? 0:a.idx>b.idx? 1:-1
+        });
+    }
+
+    let lastArr = orgData.length==0? 0: orgData[orgData.length-1].idx;
+
+    orgData.push({
+        'idx' : lastArr + 1,
+        'tit' : tit,
+        'cont' : cont
+    });
+
+    localStorage.setItem('minfo', JSON.stringify(orgData));
+
+    bindData();
+    bindMod();
+}
+
+dfn.addEvt(modSel, 'change', setMod);
+
+function setMod(){
+    let optVal = this.value;
+    if(optVal == 'show') return;
+
+    let orgData = localStorage.getItem('minfo');
+    if(!orgData){
+        localStorage.setItem('minfo', '[]');
+        orgData = localStorage.getItem('minfo');
+    }
     orgData = JSON.parse(orgData);
 
     let selRec = orgData.find(v=>{
-        if(v.idx == optVal) return true;
+        if(v.idx == optVal)return true;
     });
 
     modTit.value = selRec.tit;
     modCont.value = selRec.cont;
 }
 
-dfn.addEvt(modBtn, 'click', modifyData);
+dfn.addEvt(moBtn, 'click', modifyData);
 
 function modifyData(){
     if(modSel.value == 'show') return;
     let selIdx = modSel.value;
     let orgData = localStorage.getItem('minfo');
     if(!orgData){
-        localStorage.setItem('minfo', []);
+        localStorage.setItem('minfo', '[]');
         orgData = localStorage.getItem('minfo');
     }
-
     orgData = JSON.parse(orgData);
 
     orgData.find(v=>{
         if(v.idx == selIdx){
             v.tit = modTit.value;
-            v.xont = modCont.value;
+            v.cont = modCont.value;
         }
     })
-
     localStorage.setItem('minfo', JSON.stringify(orgData));
 
     bindData();
-
-    bindMode();
+    bindMod();
 }
--- 004.React학습/react-ex/js/pra.js
+++ 004.React학습/react-ex/js/pra.js
@@ -2,20 +2,21 @@ function initFn(){
     $('.img-box').css({
         opacity : 0
     }).delay(1000).fadeTo(500, 1);
-    $('.gwrap').hide().delay(1500).slideDown();
+    $('.gwrap').hide().delay(1500).slideDown(1000);
 }
 
 function firstOneFn(){
     $('.tit').css({
         transform : 'scale(2)',
-        transition : '1s ease-out'
-    }) 
+        transition : '1s ease-out 1s'
+    })
     setTimeout(()=>{
         $('.tit').css({
             transform : 'scale(1)',
-            transition : '1s ease-out'
+            transition : '1s ease-out 1s'
         })
-    },1500)
+
+    }, 2500)
 }
 
 export {initFn, firstOneFn};
