11782b29d53d345841add8b69c810c9fdda68ae6
ogreencakeo <magente2000@gmail.com>
2023-10-25 17:02:01 +0900

    Update 01.공유신발.jsx

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -225,21 +225,27 @@ function SubViewCode(props){
     // -> 프롭스 다운, 프롭스 펑션 업/다운
     // props.itemNum - 선택 아이템 번호
 
+    // 선택 전체 데이터
+    const selData = twoData[props.idx];
+
     // 선택데이터 /////////////////////////////////////////////
-    const selData = twoData[props.idx][Number(props.itemNum)-1];
-    // -> 전체 선택 배열 중에 [특정 배열 값 순번]
+    const selItem = selData.find(v=>{
+        if(v.idx == props.itemNum) return true;
+    })
+    // -> 고유 데이터인 idx값으로 데이터를 찾아서 
+    // 데이터를 화면에 바인딩해야 한다.
 
      // 코드 리턴 파트
     return(
         <ol>
             <li><img src={props.idx?
-                    "./images/gallery/" + selData.idx +".jpg" :
-                    "./images/vans/vans_" + selData.idx + ".jpg" }
+                    "./images/gallery/" + selItem.idx +".jpg" :
+                    "./images/vans/vans_" + selItem.idx + ".jpg" }
                     alt = {props.idx? "드레스" : "신발"} />
             </li>
             <li>
-                상품명 : {selData.gname} <br />
-                가격: {selData.gprice}원<br />
+                상품명 : {selItem.gname} <br />
+                가격: {selItem.gprice}원<br />
                 <button onClick={()=> props.chgFn(0, 0)}>리스트로 가기</button>
             </li>
         </ol>
