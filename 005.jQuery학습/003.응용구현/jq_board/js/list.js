e1b50fa68ce77e38092e431e96ad5a6ba444ca04
ogreencakeo <magente2000@gmail.com>
2023-11-01 15:18:02 +0900

    Update list.js

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -75,18 +75,40 @@ pagingBlock : ${pagingBlock}, addOver : ${addOver}`);
     const pNumBlock = $(".paging");
     let pNumCode = "";
 
+    // 새로운 블록을 위한 변수
+    let newPagingBlock;
     // 추가 리스트가 있을 경우 나머지가 0아니므로 다음 페이지 추가
-    if (addOver != 0) pagingBlock++;
+    if (addOver != 0) newPagingBlock = pagingBlock+1;
 
-    // 페이지 링크 a요소 만들기
-    for (let x = 0; x < pagingBlock; x++) {
-        pNumCode += `<a>${x + 1}</a>`;
+    // 페이지 링크 a요소 만들기 /////////
+    for (let x = 0; x < newPagingBlock; x++) {
+        // 현재 페이지만 b태그 나머지는 a태그 사용
+        // 현재 페이지는 pgNum이므로 ( x+1 == pgNum )
+        pNumCode += 
+            x+1 == pgNum? 
+            `<b>${x + 1}</b>`:
+            `<a href="#">${x + 1}</a>`;
         // 마지막 뒤에 바가 안생김
-        if (x < pagingBlock - 1) pNumCode += " | ";
-    }
+        if (x < newPagingBlock - 1) pNumCode += " | ";
+    } // for
     pNumBlock.html(pNumCode);
+
+    // 새로 생성된 a 링크에 click 이벤트 함수로
+    // 리스트 업데이트 함수 호출하기
+    $('.paging a').click(function(e){
+        // 기본 이동 막기
+        e.preventDefault();
+        // 클릭된 a요소에 숫자 읽어오기
+        let atxt = $(this).text();
+        // console.log('숫자 :', atxt);
+        // 리스트 업데이트 함수 호출
+        updateList(atxt);
+    });
+
 }; // updateList 함수
 
+// 리스트 업데이트 함수 최초 호출 : 1페이지 
+updateList(1);
 
 // 데이터 태그 생성후 태그넣기
 // board.html(
