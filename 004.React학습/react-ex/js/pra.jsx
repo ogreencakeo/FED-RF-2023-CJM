cff025b39b7ba4c45844033dae2eecc87a62fffd
jimin0427 <cjm0427@hs.ac.kr>
2023-11-02 21:10:05 +0900

    sdad

--- 004.React학습/react-ex/js/pra.jsx
+++ 004.React학습/react-ex/js/pra.jsx
@@ -5,55 +5,48 @@ import 이야기 from "./02.sub_com/sub_com";
 function 큰집(){
     const myData = mtInfo;
     const [myVal, setMyVal] = React.useState('백두산');
-    const chgMyVal = (val) => {
+    const chgmyVal = (val) => {
         setMyVal(val);
     }
     return(
-        <누구냐.Provider val={{myData, myVal, chgMyVal}}>
+        <누구냐.Provider value={{myData, myVal, chgmyVal}}>
             <할아버지 />
         </누구냐.Provider>
-    );
+    )
 }
 
 function 할아버지(){
-    return <아버지 />
-}
-
-function 아버지(){
     return <이야기 />
 }
 
-export const 누구냐 = React.createContext();
+function 이야기(){
+    const 맘대로 = React.useContext(누구냐);
+    const selData = 맘대로.myData.find(v=>{
+        if(v.이름 == 맘대로.myVal) return true;
+    });
+    const btnData = 맘대로.myData.filter(v=>{
+        if(v.이름 != 맘대로.myVal) return true;
+    })
+    return(
+        <div>
+            <h1>{맘대로.myData}</h1>
+            <ul>
+                <li>{selData.이름}</li>
+            </ul>
+            {
+                btnData.map(v=>
+                    <button onClick={()=>맘대로.chgmyVal(v.이름)}>
+                        {v.이름}
+                    </button>
+                )
+            }
+        </div>
+    )
+}
 
 ReactDOM.render(
     <큰집 />,
     document.querySelector('#root')
 )
 
-function 이야기(){
-    const 맘대로 = React.useContext(누구냐);
-    const selData = 맘대로.myData.find(v=>{
-        if(v.이름 == 맘대로.myVal) return true;
-    })
-    const btnData = 맘대로.myData.find(v=>{
-        if(v.이름 != 맘대로.myVal) return true;
-    })
-    return(
-        <div>
-            <h1>{맘대로.myVal}</h1>
-            <img src={selData.이미지} alt={selData.이름} />
-            <div>
-                <ul>
-                    <li>{selData.이름}</li>
-                </ul>
-            </div>
-            {
-                btnData.map(v=>{
-                    <button onClick={()=>맘대로.chgMyVal(v.이름)}>
-                        {v.이름}
-                    </button>
-                })
-            }
-        </div>
-    )
-}
+export const 누구냐 = React.useContext();
--- 005.jQuery학습/003.응용구현/jq_board/js/pra.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/pra.js
@@ -1,14 +1,61 @@
-import bData from "./data.json" assert { type: "json" };
-bData.sort((a, b) => Number(a.idx) == Number(b.idx)? 0 : Number(a.idx) > Number(b.idx)? -1:1);
+import bData from './data.json' assert { type : 'json'};
+
+bData.sort(Number(a.idx) == Number(b.idx)? 0 :
+Number(a.idx) > Number(b.idx)? -1:1);
+
+let myData = JSON.stringify(bData);
+
+if(!localStorage.getItem('boardData')){
+    localStorage.setItem('boardData', myData);
+}
+
+let useData = localStorage.getItem('boardData');
+useData = JSON.parse(useData);
 
 const board = $('#board tbody');
+
 let listNum = 0;
+
 const addNum = () => ++listNum;
 
 const pgBlock = 9;
-let pgNum = 1;
+const pgNum = 1;
 const totalCnt = bData.length;
 let pagingBlock = Math.floor(totalCnt / pgBlock);
 let addOver = totalCnt % pgBlock;
 
-const updateList = ()
+export const updateList = (newPgNum) => {
+    pgNum = newPgNum;
+
+    listNum = (pgNum-1) * pgBlock;
+    for(let i=(pgNum-1)*pgBlock; i<pgBlock*pgNum; i++){
+        if(i>=totalCnt) break;
+        hcode += `
+            <tr>
+                <td>${addNum()}</td>
+                <td>${bData[i].tit}</td>
+                <td>${bData[i].writer}</td>
+                <td>${bData[i].date}</td>
+                <td>${bData[i].cnt}</td>
+            </tr>
+
+        `;
+    }
+    board.html(hcode);
+
+    const pNumBlock = $('.paging');
+    let pNumCode = '';
+    let newPagingBlock;
+
+    if(addOver!=0) newPagingBlock = pagingBlock + 1;
+    for(let x=0; x<newPagingBlock; x++){
+        pNumCode += x + 1 ==pgNum ? `<b>${x+1}</b>` : `<a href="#">${x+1}</a>`;
+        if(x < newPagingBlock - 1) pNumCode += ' | ';
+    }
+    $('.paging a').click(function(e){
+        e.preventDefault();
+        let atxt = $(this).text();
+        updateList(atxt);
+    })
+};
+
