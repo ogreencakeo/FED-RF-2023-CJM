f70eb4cd45de31662c504f45742713de9b2a391e
ogreencakeo <magente2000@gmail.com>
2023-11-02 17:19:18 +0900

    asdasd

--- 005.jQuery학습/003.응용구현/jq_board/js/update_list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/update_list.js
@@ -1,5 +1,68 @@
 // 리스트 업데이트 JS - update_list.js
-const updateList = (newPgNum) => {
+
+
+// 게시판 데이터 불러오기
+import bData from "./data.json" assert { type: "json" };
+
+// console.log('bData :', bData);
+
+
+// 데이터 idx의 내림차순으로 정렬변경하기
+bData.sort((a, b) => (Number(a.idx) == Number(b.idx) ? 0 : Number(a.idx) > Number(b.idx) ? -1 : 1));
+// idx는 숫자데이터로 형변환 비교하여
+// a.idx>b.idx 즉, 앞에 것이 크면 그대로 -1, 작으면 바꾸기 1
+console.log(Array.isArray(bData));
+
+// 원본 배열 데이터를 문자형 데이터로 변경!
+let myData = JSON.stringify(bData)
+console.log(myData);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// 로컬 스토리지 생성하기 /////////////////
+// 이름 boardData 
+if(!localStorage.getItem('boardData')){
+    localStorage.setItem('boardData', myData);
+}
+// console.log(localStorage.getItem('boardData'));
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+let useData = localStorage.getItem('boardData');
+useData = JSON.parse(useData);
+console.log('변환후 useData :', useData);
+
+// 데이터를 화면 리스트 코드로 변환하여 적용한다.
+// 대상 : #board tbody
+const board = $("#board tbody");
+
+// 리스트 번호변수
+let listNum = 0;
+
+// 숫자1씩 증가 함수
+const addNum = () => ++listNum;
+// console.log('증가수 addNum() :', addNum());
+
+// 페이징 되는 리스트 만들기 ////////////////////////////////////
+// 페이징의 기본 원리 :
+// 1. 한 페이지당 리스트 수를 정하여 전체 페이지 개수를 구하기
+// 2. 개수만큼 리스트에 데이터를 반복문으로 생성함
+// 3. 페이지 번호에 따라 시작번호를 업데이트 한다.
+
+// 페이징 관련 변수들 /////////////////////////////////////////
+// [1] 한 페이지당 리스트 수 : pgBlock
+const pgBlock = 9;
+// [2] 페이지 순번 : pgNum -> 증감예정
+let pgNum = 1;
+// [3] 전체 레코드 수 : totalCnt
+const totalCnt = bData.length;
+// [4] 페이징 블록 계산하기
+let pagingBlock = Math.floor(totalCnt / pgBlock);
+// [5] 나머지 리스트 여부 : 0이면 다음 페이지 없음!
+let addOver = totalCnt % pgBlock;
+
+//////////////// 여기서부터 업데이트가 페이지별로 반복됨 ////////
+
+export const updateList = (newPgNum) => {
     // newPgNum -  새롭게 전달되는 현재 페이지 번호
     pgNum = newPgNum; // 기존 페이지 번호를 업데이트 함
 
