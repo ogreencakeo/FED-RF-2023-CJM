8be96e20c33f6a8587d03893d2640a6740a1c660
ogreencakeo <magente2000@gmail.com>
2023-11-03 11:58:08 +0900

    fsd

--- 005.jQuery학습/003.응용구현/jq_board/js/pra.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/pra.js
@@ -1,61 +1,22 @@
-import bData from './data.json' assert { type : 'json'};
+$(()=>{
+    const updataWin = () => $(window).width();
+    let winW = updataWin();
 
-bData.sort(Number(a.idx) == Number(b.idx)? 0 :
-Number(a.idx) > Number(b.idx)? -1:1);
+    const TRS_TIME_DT = '.8s ease-out';
+    const TRS_TIME_MOB = '.3s ease-out';
 
-let myData = JSON.stringify(bData);
+    const target = $('#move');
 
-if(!localStorage.getItem('boardData')){
-    localStorage.setItem('boardData', myData);
-}
-
-let useData = localStorage.getItem('boardData');
-useData = JSON.parse(useData);
-
-const board = $('#board tbody');
-
-let listNum = 0;
-
-const addNum = () => ++listNum;
-
-const pgBlock = 9;
-const pgNum = 1;
-const totalCnt = bData.length;
-let pagingBlock = Math.floor(totalCnt / pgBlock);
-let addOver = totalCnt % pgBlock;
-
-export const updateList = (newPgNum) => {
-    pgNum = newPgNum;
-
-    listNum = (pgNum-1) * pgBlock;
-    for(let i=(pgNum-1)*pgBlock; i<pgBlock*pgNum; i++){
-        if(i>=totalCnt) break;
-        hcode += `
-            <tr>
-                <td>${addNum()}</td>
-                <td>${bData[i].tit}</td>
-                <td>${bData[i].writer}</td>
-                <td>${bData[i].date}</td>
-                <td>${bData[i].cnt}</td>
-            </tr>
-
-        `;
-    }
-    board.html(hcode);
-
-    const pNumBlock = $('.paging');
-    let pNumCode = '';
-    let newPagingBlock;
-
-    if(addOver!=0) newPagingBlock = pagingBlock + 1;
-    for(let x=0; x<newPagingBlock; x++){
-        pNumCode += x + 1 ==pgNum ? `<b>${x+1}</b>` : `<a href="#">${x+1}</a>`;
-        if(x < newPagingBlock - 1) pNumCode += ' | ';
-    }
-    $('.paging a').click(function(e){
-        e.preventDefault();
-        let atxt = $(this).text();
-        updateList(atxt);
+    target.draggable({
+        asix : 'x'
     })
-};
+    .css({
+        transtion : 
+        (winW < 500? TRS_TIME_MOB : TRS_TIME_DT)
+    });
 
+    $(window).resize(()=>{
+        winW = updataWin();
+        
+    });
+})
