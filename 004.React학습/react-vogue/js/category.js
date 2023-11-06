954817ea8146ab2ccce1332904c3d4ab15f7efbe
ogreencakeo <magente2000@gmail.com>
2023-11-06 16:20:04 +0900

    sdgsd

--- 004.React학습/react-vogue/js/category.js
+++ 004.React학습/react-vogue/js/category.js
@@ -5,8 +5,12 @@
 // 상단영역 컴포넌트 불러오기 /////
 import TopArea from "./components/top_area.jsx";
 
+// 메인영역 컴포넌트 불러오기
+import MainCategory from './components/main_area.jsx';
+
 // 하단영역 컴포넌트 불러오기 /////
 import FooterArea from "./components/footer_area.jsx";
+
 ////// 카테고리 페이지 메인 컴포넌트 ///////
 /******************************************* 
     컴포넌트명 : MainComponent
@@ -15,7 +19,12 @@ import FooterArea from "./components/footer_area.jsx";
 function MainComponent(){
     return(
         <React.Fragment>
+            {/* 1. 상단영역 */}
             <TopArea />
+            {/* 2. 메인영역 */}
+            <MainCategory />
+            {/* 3. 하단영역 */}
+            <FooterArea />
         </React.Fragment>
     );
 } // MainComponent /////////////////////////////////////
