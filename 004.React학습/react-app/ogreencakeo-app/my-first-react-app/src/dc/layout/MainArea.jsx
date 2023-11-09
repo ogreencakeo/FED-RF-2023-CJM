9346ed540046b0fd31c1a34e04f40d7eca71cdd1
ogreencakeo <magente2000@gmail.com>
2023-11-09 16:58:58 +0900

    sfd
    fsd

--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/layout/MainArea.jsx
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/layout/MainArea.jsx
@@ -1,10 +1,21 @@
 // MainArea 컴포넌트
 import { Banner } from "../contents/Banner";
+import { Character } from "../contents/Character";
+import { Main } from "../contents/Main";
+import { Video } from "../contents/Video";
 
-export function MainArea(){
+export function MainArea(props){
+    // cat 속성으로 메뉴 분류 전달
+
     return(
         <main className="cont">
-            <Banner category="main" />
+            {
+                props.cat == 'main' &&
+                <Main cat={props.cat} />
+            }
+            {props.cat == 'CHARACTERS' &&
+            <Character cat={props.cat} />}
+            {props.cat == 'VDIEO' && <Video />}
         </main>
     );
 } ////// MainArea 컴포넌트 ///////
