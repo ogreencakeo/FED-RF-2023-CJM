1c37970a4cad2d63d941b73a95057f70e03cfc4b
ogreencakeo <magente2000@gmail.com>
2023-11-10 13:18:50 +0900

    Update SwiperApp.jsx

--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -4,15 +4,15 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
+// import "swiper/css";
+// import "swiper/css/pagination";
 
 // import "./styles.css";
 
 // import required modules
 import { Pagination } from "swiper/modules";
 
-export default function SwiperApp() {
+export function SwiperApp() {
     return (
         <>
             <Swiper
