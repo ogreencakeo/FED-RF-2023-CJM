dd7730253d2d9871339abaddc08dc7bc8ce58d62
ogreencakeo <magente2000@gmail.com>
2023-11-10 15:11:45 +0900

    Update SwiperApp.jsx

--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -11,6 +11,8 @@ import 'swiper/css/navigation';
 import "./css/swiper.css";
 
 // import required modules
+// 사용할 스와이퍼 모듈을 불러온다. 
+// (여기서는 페이지네이션, 네비게이션, 자동넘김)
 import { Pagination,Navigation, Autoplay } from "swiper/modules";
 
 export function SwiperApp() {
@@ -28,6 +30,7 @@ export function SwiperApp() {
                 }}
                 loop={true}
                 navigation={true}
+                // 사용할 모듈을 여기에 적용시킨다.
                 modules={[Pagination, Navigation, Autoplay]}
                 className="mySwiper"
                 autoplay ={{
