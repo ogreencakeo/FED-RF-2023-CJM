c22308b2b91ad722d372f1993258d0b5f1f3304f
ogreencakeo <magente2000@gmail.com>
2023-11-10 16:15:12 +0900

    sdf

---
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/layout/Layout.jsx
@@ -0,0 +1,12 @@
+// DC.com 레이아웃 컴포넌트
+import { TopArea } from './TopArea';
+import { MainArea } from './MainArea';
+import { FooterArea } from './FooterArea';
+
+export function Layout(){
+    <>
+        <TopArea />
+        <MainArea />
+        <FooterArea />
+    </>
+} // Layout 컴포넌트 /////////////////
--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/index.js
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/index.js
@@ -1,34 +1,27 @@
 // index.js는 public/index.html 페이지에 적용되는 컴포넌트다!->루트 컴포넌트
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 // css 도 불러온다!
 import './css/index.css';
+import { Layout } from './dc/layout/Layout';
 
-import { TopArea } from './dc/layout/TopArea';
-import { MainArea } from './dc/layout/MainArea';
-import { FooterArea } from './dc/layout/FooterArea';
-
-function App(){
-  // 상단메뉴 클릭시 메인컨텐츠 변경을 위해 
-  // 후크관리변수를 생성한다.
-  const [menu, setMenu] = useState('main');
-
-  // 메뉴 업데이트 함수
-  const chgMenu = (txt) => {
-    console.log('메뉴 업데이트!', txt);
-    // 상태관리변수 변경
-    setMenu(txt);
-  };  // chgMenu 함수 ////////////
+// 라우터 구성 컴포넌트 : 스스로 내보내기 셋팅 필수
+// 레이아웃 컴포넌트를 라우터에 입혀서 화면에
+// 풀력해야하기 때문에 스스로 내보내기를 셋팅해야 하는 것!
+export default function App(){
 
   return(
-    <>
-      <TopArea chgFn={chgMenu} />
-      <MainArea cat={menu} />
-      <FooterArea />
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+} // App 컴포넌트 ////////////////
 
 // 컴포넌트 출력 //////////
 // 먼저 root객체만들고
