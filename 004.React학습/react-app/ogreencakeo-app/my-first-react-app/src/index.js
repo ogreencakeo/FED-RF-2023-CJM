98a36775fe8fde902236b1bd812cf7ad33310d64
ogreencakeo <magente2000@gmail.com>
2023-11-10 16:20:18 +0900

    Update index.js

--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/index.js
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/index.js
@@ -6,6 +6,11 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 // css 도 불러온다!
 import './css/index.css';
 import { Layout } from './dc/layout/Layout';
+import { Main } from './dc/contents/Main';
+import { Character } from './dc/contents/Character';
+import { Comics } from './dc/contents/Comics';
+import { Movies } from './dc/contents/Movies';
+import { SwiperApp } from './dc/plugin/SwiperApp';
 
 // 라우터 구성 컴포넌트 : 스스로 내보내기 셋팅 필수
 // 레이아웃 컴포넌트를 라우터에 입혀서 화면에
@@ -15,8 +20,17 @@ export default function App(){
   return(
     <BrowserRouter>
       <Routes>
+        {/* 중요! 레이아웃 컴포넌트를 루트로 설정! */}
         <Route path='/' element={<Layout />}>
-
+          {/* 하위 라우트 셋팅 */}
+          <Route index element = {<Main />} />
+          <Route path='character' element = {<Character />} />
+          <Route path='comics' element = {<Comics />} />
+          <Route path='movies' element = {<Movies />} />
+          <Route path='games' element = {<Games />} />
+          <Route path='news' element = {<News />} />
+          <Route path='video' element = {<Video />} />
+          <Route path='swiperapp' element = {<SwiperApp />} />
         </Route>
       </Routes>
     </BrowserRouter>
