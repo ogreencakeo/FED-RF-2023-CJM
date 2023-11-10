0f3de3f33a8bb1e051abc3211766dab1c88566c0
ogreencakeo <magente2000@gmail.com>
2023-11-10 10:20:02 +0900

    Update Banner.jsx

--- 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/contents/Banner.jsx
+++ 004.React학습/react-app/ogreencakeo-app/my-first-react-app/src/dc/contents/Banner.jsx
@@ -5,10 +5,17 @@ import { banData } from "../data/banner";
 
 // 배너CSS
 import '../../css/banner.css';
+import { useEffect } from "react";
 
 // 배너 컴포넌트 /////////////
 export function Banner(props){
     // category - 카테고리 분류명(배너 데이터 선택)
+
+    // 페이지 랜더링 후 실행구역
+    useEffect(()=>{
+        
+    });
+
     // 리스트 만들기 함수
     const makeList = (data) => {
         console.log(data);
