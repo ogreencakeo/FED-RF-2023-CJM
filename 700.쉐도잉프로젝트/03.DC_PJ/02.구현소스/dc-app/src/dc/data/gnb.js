3a58b9fbe7ee0687f041d3e7297664eac353765d
ogreencakeo <magente2000@gmail.com>
2023-11-13 12:33:17 +0900

    hb

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/dc/data/gnb.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/dc/data/gnb.js
@@ -1,9 +1,9 @@
 // DC.com GNB메뉴 데이터
 export const menu = [
-    {
-        txt:"Home",
-        link:"/",
-    },
+    // {
+    //     txt:"Home",
+    //     link:"/",
+    // },
     {
         txt:"CHARACTERS",
         link:"/character",
@@ -11,7 +11,9 @@ export const menu = [
     {
         txt:"COMICS",
         link:"/comics",
-        sub:[{txt:"Hi",link:"/hi"},{txt:"My",link:"/my"},{txt:"We",link:"/we"},],
+        sub:[
+            
+        ],
     },
     {
         txt:"MOVIES",
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/dc/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/dc/layout/TopArea.jsx
@@ -4,15 +4,6 @@ import { Link } from "react-router-dom";
 import { Logo } from "../contents/Logo";
 import { menu } from "../data/gnb";
 
-/******************************************************* 
-    [ 리액트 라우터와 연결하여 사용되는 라우터 컴포넌트 ]
-    1. <Link to="/경로명"></Link>
-    -> to속성의 경로는 <Route path="/경로명"> 과 일치함!
-
-    2. <Outlet />
-    -> 라우터 연결 컴포넌트 출력자리 컴포넌트
-    -> 여기서는 MainArea 컴포넌트에 출력!
-*******************************************************/
 export function TopArea(){
 
     return(
@@ -29,25 +20,7 @@ export function TopArea(){
                         menu.map((v, i)=>
                             <li key={i}>
                                 <Link to = {v.link}>{v.txt}</Link>
-                            {/* 
-                                <a href="#"
-                                onClick={() => 
-                                    props.chgFn(
-                                        v.txt == 'Home'? 'main' : v.txt
-                                    )}>
-                                    {v.txt}
-                                </a> 
-                            */}
                             </li>
-                            /*
-                                map()을 사용하여 태그를 생성할 때
-                                데이터의 유일키를  key속성으로 만들지 않으면 
-                                아래와 같은 에러가 발생함
-                                (이유 : 구별되는 항목으로 나중에 업데이트 시
-                                    이용할 수 있도록 리액트에서 강제하고 있음!)
-                                Waring : Each child in a list
-                                should have a unique "key" prop.
-                            */
                         )
                     }
                 </ul>
