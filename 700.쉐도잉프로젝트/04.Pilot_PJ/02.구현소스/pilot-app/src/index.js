4cce71127afc17edddf39fb8d69d7184b9735c28
ogreencakeo <magente2000@gmail.com>
2023-11-13 15:56:44 +0900

    Update index.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,11 +1,16 @@
 // 메인 페이지 JS - index.js
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import { TopArea } from './layout/topArea';
+import { TopArea } from './layout/TopArea';
 import { MainArea } from './layout/MainArea';
 import { FooterArea } from './layout/FooterArea';
 
+// 최상위 Root 컴포넌트 //////////
 function App(){ 
+
+  // 후크상태변수 설정 : 페이지변경
+  const [pgName]
+
   return(
     <>
       <TopArea />
