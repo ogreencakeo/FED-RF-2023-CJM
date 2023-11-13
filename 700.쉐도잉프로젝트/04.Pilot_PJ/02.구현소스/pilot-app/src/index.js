60e6889871c968b2dae064932c142ca75c0090e6
ogreencakeo <magente2000@gmail.com>
2023-11-13 16:06:37 +0900

    sdf

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,5 +1,5 @@
 // 메인 페이지 JS - index.js
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { TopArea } from './layout/TopArea';
 import { MainArea } from './layout/MainArea';
@@ -9,12 +9,21 @@ import { FooterArea } from './layout/FooterArea';
 function App(){ 
 
   // 후크상태변수 설정 : 페이지변경
-  const [pgName]
+  const [pgName, setPgName] = useState('main');
+
+  // 페이지 변경 상태변수 업데이트 함수
+  const chgPgName = (txt) => {
+    setPgName(txt)
+  }; // chgPgName 함수 ////////
 
   return(
     <>
       <TopArea />
-      <MainArea page='main'/>
+      <button onClick={()=>chgPgName('main')}>메인 페이지</button>
+      <button onClick={()=>chgPgName('men')}>남성 페이지</button>
+      <button onClick={()=>chgPgName('women')}>여성 페이지</button>
+      <button onClick={()=>chgPgName('style')}>스타일 페이지</button>
+      <MainArea page={pgName} />
       <FooterArea />
     </>
   )
