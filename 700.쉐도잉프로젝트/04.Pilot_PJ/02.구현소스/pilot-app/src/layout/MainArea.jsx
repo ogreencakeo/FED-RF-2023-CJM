c645d42ab332ee4fdc9b2a24f656a8e84805c661
ogreencakeo <magente2000@gmail.com>
2023-11-13 15:19:20 +0900

    sdf

---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -0,0 +1,8 @@
+// Pilot PJ 메인영역 공통 컴포넌트
+export function MainArea(){
+    return(
+        <>
+            <h1>메인영역</h1>
+        </>
+    );
+} // MainArea 컴포넌트 /////////////
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/TopArea.jsx
@@ -1,4 +1,8 @@
 // Pilot PJ 상단영역 공통 컴포넌트
 export function TopArea(){
-
+    return(
+        <>
+            <h1>상단영역</h1>
+        </>
+    );
 } // TopArea 컴포넌트 /////////////
