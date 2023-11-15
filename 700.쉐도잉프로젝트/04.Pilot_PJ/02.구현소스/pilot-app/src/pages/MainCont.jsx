158d600c81a4484699365c4379643260aa6c3866
ogreencakeo <magente2000@gmail.com>
2023-11-15 16:20:12 +0900

    Update MainCont.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -19,11 +19,18 @@ export function MainCont(){
         
         // 대상 .slide
         const slide = $('.slide');
+        // 커버
+        const cover = $('.cover');
+
         // 드래그 기능 넣기
         slide.draggable({axis : 'x'});
         
         // 드래그가 끝났을 때 슬라이드 위치
         slide.on('dragstop', ()=>{
+
+            // 광드래그 막기 커버
+            $('.cover').show();
+
             // 비교를 위한 윈도우 가로값
             let winW = $(window).width();
             // 현재 슬라이드 left 값
@@ -31,10 +38,13 @@ export function MainCont(){
             // 이동차이수 = 현재 슬라이드 위치값 (양수) - 윈도우 가로값
             let diff = Math.abs(pos) - winW;
             // 결과해석 : 양수-> 왼쪽으로 이동 / 음수 - 오른쪽으로 이동
+
+            // 기준값 : 화면크기를 기준한 부분
+            let gap = winW/10;
             console.log('드래그 멈춰(pos) :', pos, ', winW :', winW, ', diff :', diff);
             
             // 왼쪽으로 이동하기
-            if(diff > winW/10){
+            if(diff > gap){
                 slide.animate({left : '-200%'}, 800, 'easeOutQuint',
                 ()=>{
                     // 맨앞 li 맨뒤로 이동
@@ -44,7 +54,7 @@ export function MainCont(){
                 })
             }
             // 오른쪽으로 이동하기
-            else if(diff < -winW/10){
+            else if(diff < -gap){
                 slide.animate({left : '0'}, 800, 'easeOutQuint', 
                 ()=>{
                     // 맨뒤 li 맨앞으로 이동
