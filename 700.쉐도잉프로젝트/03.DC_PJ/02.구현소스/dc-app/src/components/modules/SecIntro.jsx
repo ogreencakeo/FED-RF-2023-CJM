41c42b6ea55b71d5d2f9f862211bfd71093396ce
ogreencakeo <magente2000@gmail.com>
2023-11-15 12:18:40 +0900

    sfd

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -0,0 +1,8 @@
+// DC.com 섹션소개 컴포넌트 : SecIntro
+export function SecIntro(){
+    return(
+        <>
+            
+        </>
+    );
+}  // SecIntro 컴포넌트 /////////////////////
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,6 +1,7 @@
 // 메인페이지 메인컨텐츠
 
 import { Banner } from "../modules/Banner";
+import { SecIntro } from "../modules/secIntro";
 
 export function Main(/*props*/){
     // cat 속성으로 메뉴분류 전달
@@ -11,6 +12,10 @@ export function Main(/*props*/){
             결과적으로 'main1'/'main2'/'main3'중 하나를 불러온다. */}
             <Banner category={"main" + Math.ceil(Math.random()*3)} />
             {/* <Banner category={props.cat} /> */}
+
+            {/* 2. 섹션 소개 컴포넌트 */}
+            <SecIntro />
+
         </>
     );
 } /////////////// Main 컴포넌트 ////////
