3ae1aa976c77d44ffc52952808a4a753b025de73
ogreencakeo <magente2000@gmail.com>
2023-11-15 12:11:23 +0900

    asd

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 import { Logo } from "../modules/Logo";
 import { menu } from "../data/gnb";
 
+// 폰트어썸 불러오기
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function TopArea(){
 
     return(
@@ -13,9 +17,11 @@ export function TopArea(){
             {/* 네비게이션 GNB파트 */}
             <nav className="gnb">
                 <ul>
+                    {/* 1. 로고 컴포넌트 */}
                     <li>
                         <Logo logoStyle='top' />
                     </li>
+                    {/* 2. GNB메뉴 데이터 기반으로 li태그 생성하기 */}
                     {
                         menu.map((v, i)=>
                             <li key={i}>
@@ -39,7 +45,23 @@ export function TopArea(){
                             </li>
                         )
                     }
+                    {/* 3. 검색, 회원가입, 로그인 링크 */}
+                    <li style={{marginLeft : 'auto'}}>
+                        {/* 검색기능링크 - 클릭시 검색창 보이기 */}
+                        <a href="#" onClick="">
+                            <FontAwesomeIcon icon={faSearch} />
+                        </a>
+                    </li>
+                    {/* 회원가입, 로그인은 로그인 아닌 상태일때 나옴 */}
+                    <li>
+                        <Link to='/member'>JOIN US</Link>
+                    </li>
+                    <li>
+                        <Link to ='/login'>LOGIN</Link>
+                    </li>
                 </ul>
+                {/* 모바일용 햄버거 버튼 */}
+                <button className="hambtn" onClick=""></button>
             </nav>
         </header>
         </>
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -6,7 +6,7 @@ export function Main(/*props*/){
     // cat 속성으로 메뉴분류 전달
     return(
         <>
-            <h1 style={{textAlign : 'center'}}>메인 페이지</h1>
+            {/* 1. 배너 컴포넌트 */}
             {/* Math.ceil(Math.random()*3) : 1~3중 임의의 난수를 발생함 
             결과적으로 'main1'/'main2'/'main3'중 하나를 불러온다. */}
             <Banner category={"main" + Math.ceil(Math.random()*3)} />
