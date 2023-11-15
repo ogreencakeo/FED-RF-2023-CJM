57a5a152f1154714f68a3b86ece6ec57c2f9ccd4
ogreencakeo <magente2000@gmail.com>
2023-11-15 11:10:39 +0900

    das

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
@@ -1,16 +1,27 @@
 // DC.com 로고 컴포넌트
 import React from "react";
 import { isrc } from "../data/imgSrc";
+import { useNavigate } from "react-router-dom";
 
 export const Logo = (props) => {
     // props.logoStyle : 상단, 하단구분 로고 코드
+    
+    // 라우터 이동 메서드 셋팅하기 : useNavigate()
+    const goNav = useNavigate();
+    // 사용법 : 반드시 useNavigate() 메서드를 변수에 담아
+    // 이동할 라우터 주소를 쓰면 이동한다.
+    // 예) goNav('/news') -> 뉴스 페이지 이동
+    // 예) goNav('/') -> 첫 페이지 이동
+    // 이동 주소는 대소문자 구분 없음!
+
     // 객체형 스타일 적용
     const myStyle = {
         top : {
             width:"45px",
             height:"45px",
             marginRight : '30px', 
-            borderRadius:"50%"
+            borderRadius:"50%",
+            cursor : 'pointer',
         },
         bottom : {
             height : '80px',
@@ -19,7 +30,9 @@ export const Logo = (props) => {
 
     // 자식컴포넌트 처리용 함수
     const nayaLogo = (txt) => {
-        console.log(txt);
+        // console.log(txt);
+        // 라우터 이동하기
+        goNav('/');
     }; // nayaLogo
 
     // 이미지 스타일 변경 객체
