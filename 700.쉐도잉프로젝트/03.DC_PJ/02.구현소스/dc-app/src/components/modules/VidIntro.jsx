1b6dc6091d4675971c415a6e1f46fb45b33c57fc
ogreencakeo <magente2000@gmail.com>
2023-11-16 14:33:48 +0900

    Update VidIntro.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -19,6 +19,19 @@ export function VidIntro(props) {
     // 선택데이터 : 페이지 분류명으로 선택
     const selData = vidIntroData[props.cat];
 
+    // 링크 생성 함수 : desc 데이터  / sum 데이터에서 처리
+    const linkCode = (data) => {
+        return (
+            <>
+                {data.split("*")[0]}
+                <a href={selData.link[1]} target="_blank">
+                    {selData.link[0]}
+                </a>
+                {data.split("*")[1]}
+            </>
+        );
+    }; // linkCode함수 //////////////
+
     return (
         /* props.cls로 전달되는 값이 'on' / 'off'임 */
         <section className={"vidbox" + " " + props.cls}>
@@ -33,12 +46,22 @@ export function VidIntro(props) {
                     <h3>{selData.stit}</h3>
                     {/* 큰 제목 */}
                     <h2>{selData.btit}</h2>
-                    {/* 요약소개 */}
+                    {/* 요약소개 : sum */}
                     <p>{selData.sum}</p>
-                    {/* 설명(링크포함) */}
-                    <p>{selData.desc}</p>
+                    
+                    {/* 설명(링크포함) : desc */}
+                    <p>
+                        {/* 특수문자(*) 여부에 따라 처리
+                        indexof(문자열) -> 없으면 -1 리턴 */}
+                        {selData.desc.indexOf("*") == -1 ? selData.desc : linkCode(selData.desc)}
+                    </p>
+                    {/* 설명(링크포함) : sum */}
+                    <p>
+                        {/* 특수문자(*) 여부에 따라 처리
+                        indexof(문자열) -> 없으면 -1 리턴 */}
+                        {selData.sum.indexOf("*") == -1 ? selData.sum : linkCode(selData.sum)}
+                    </p>
                 </div>
-                {/* 3.  파트 */}
             </div>
         </section>
     );
