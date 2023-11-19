b301735afa7a411febc47ba95d6cf707faaf3878
jimin0427 <cjm0427@hs.ac.kr>
2023-11-19 22:24:40 +0900

    asd

---
+++ pratice/my-first-react-app/src/dc/modules/SecIntro.jsx
@@ -0,0 +1,32 @@
+import { secIntroData } from "../data/sec_intro";
+import '../../css/sec_intro.css';
+import { useContext } from "react";
+import { dcCon } from "./dcContext";
+export function SecIntro(){
+    const myCon = useContext(dcCon);
+    const selData = secIntroData;
+    return(
+        <>
+            <section className="sec-intro">
+                {
+                    selData.map((v, i) =>
+                        <div key={i}>
+                            <div className="imbx">
+                                <img src={v.isrc} alt={v.tit.split('^')[0]} />
+                                <div className="titbx">
+                                    <h3>{v.tit.split('^')[0]}</h3>
+                                    <h3>{v.tit.split('^')[1].toUpperCase()}</h3>
+                                </div>
+                                <div className="btnbx">
+                                    <button onClick={()=>myCon.chgPage(v.link)}>
+                                        {v.btn.toUpperCase()}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </section>
+        </>
+    )
+}
