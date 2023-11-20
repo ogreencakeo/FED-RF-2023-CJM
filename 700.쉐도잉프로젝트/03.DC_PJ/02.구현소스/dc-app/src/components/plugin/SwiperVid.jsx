bf499990e32738d866de40cc22a15e8c092eb5f0
ogreencakeo <magente2000@gmail.com>
2023-11-20 12:21:29 +0900

    Update SwiperVid.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 /* 제이쿼리 넣기 */
-import $ from 'jquery';
+import $ from "jquery";
 
 // SwiperVid 사용 데이터 가져오기
 import { swVidData } from "../data/swiper_vid";
@@ -12,7 +12,7 @@ import { swVidData } from "../data/swiper_vid";
 // Import Swiper styles
 import "swiper/css";
 // 양쪽 이동버튼만 필요함!
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 
 /* 폰트어썸 임포트 */
 import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
@@ -22,18 +22,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./css/swiper_vid.css";
 
 // import required modules
-// 사용할 스와이퍼 모듈을 불러온다. 
+// 사용할 스와이퍼 모듈을 불러온다.
 // (여기서는 페이지네이션, 네비게이션, 자동넘김)
 import { Navigation } from "swiper/modules";
 
 export function SwiperVid() {
-
     // 선택 데이터 : 여기서는 그대로 가져옴
     const selData = swVidData;
 
+    // 비디오 보이기 함수
+    const showVid = (src, tit) => {
+        // src - 비디오 경로, tit - 비디오 제목,
+        console.log(src, tit);
+        // 1. 대상선정
+        // 1-1. 아이프레임 : .play-vid iframe
+        const ifr = $('.play-vid iframe');
+        // 1-2. 전체 박스 : .vid-bx
+        const vbx = $('.vid-bx');
+        // 1-2. 타이틀 박스 : .ifr-tit
+        const itit = $('.ifr-tit');
+
+        // 2. 변경하기
+        // 2-1. 아이프레임 src 경로 넣기
+        ifr.attr('src', src);
+        // 2-2. 아이프레임 비디오 타이틀 넣기
+        itit.text(tit);
+        // 2-3. 비디오 전체 박스 나타나기
+        vbx.fadeIn(300);
+
+    }; // showVid 함수 ////////////////
+
+    // 리턴코드 //////////////////////
     return (
         <>
-            <Swiper 
+            <Swiper
                 slidesPerView={4}
                 spaceBetween={20}
                 navigation={true}
@@ -41,32 +63,33 @@ export function SwiperVid() {
                 modules={[Navigation]}
                 className="mySwiper"
             >
-                {
-                    selData.map((v, i) =>
-                        <SwiperSlide key={i}>
-                            <section className="sw-inbox">
-                                {/* 동영상 이미지 박스 */}
-                                <div className="vid-img">
-                                    <img src={v.isrc} alt={v.tit} />
-                                    {/* 폰트어썸 아이콘 */}
-                                    <FontAwesomeIcon icon={faCirclePlay}
+                {selData.map((v, i) => (
+                    <SwiperSlide key={i}>
+                        {/* 비디오 보이기 함수 호출 ( 비디오 경로, 제목을 보내줌! ) */}
+                        <section className="sw-inbox" onClick={()=>showVid(v.vsrc, v.tit)}>
+                            {/* 동영상 이미지 박스 */}
+                            <div className="vid-img">
+                                <img src={v.isrc} alt={v.tit} />
+                                {/* 폰트어썸 아이콘 */}
+                                <FontAwesomeIcon
+                                    icon={faCirclePlay}
                                     style={{
-                                        position : 'absolute',
-                                        bottom : '55%',
-                                        left : '10%',
-                                        color : '#fff',
-                                        fontSize : '50px'
-                                    }} />
-                                </div>
-                                {/* 동영상 타이틀 박스 */}
-                                    <div className="vid-tit">
-                                        <h4>{v.cat}</h4>
-                                        <h3>{v.tit}</h3>
-                                    </div>
-                            </section>
-                        </SwiperSlide>
-                    )
-                }
+                                        position: "absolute",
+                                        bottom: "55%",
+                                        left: "10%",
+                                        color: "#fff",
+                                        fontSize: "50px",
+                                    }}
+                                />
+                            </div>
+                            {/* 동영상 타이틀 박스 */}
+                            <div className="vid-tit">
+                                <h4>{v.cat}</h4>
+                                <h3>{v.tit}</h3>
+                            </div>
+                        </section>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </>
     );
