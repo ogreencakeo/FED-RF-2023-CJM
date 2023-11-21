d6facd84fa0daaebe85b969d895948ca9744bf9f
ogreencakeo <magente2000@gmail.com>
2023-11-21 13:17:57 +0900

    k;ml

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -1,48 +1,59 @@
 /* DC PJ 캐릭터 리스트용 스와이퍼 컴포넌트 */
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// 캐릭터 리스트 데이터 가져오기
+import { catListData } from "../data/swiper_cat";
+
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
 import 'swiper/css/navigation';
 
-import "./css/swiper.css";
+import "./css/swiper_cat.css";
 
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다. 
 // (여기서는 페이지네이션, 네비게이션, 자동넘김)
-import { Pagination,Navigation, Autoplay } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 
 export function SwiperCat() {
 
-    // 불러올 이미지 리스트
-    const imgArr = ["dcm28", "dcm29", "dcm30", "dcm31", "dcm32", "dcm10", "dcm11", "dcm12", ];
+    // 선택 데이터
+    const selData = catListData;
 
     return (
         <>
             <Swiper 
-                slidesPerView={3}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                loop={true}
+                // slidesPerView={7}
+                spaceBetween={20}
                 navigation={true}
                 // 사용할 모듈을 여기에 적용시킨다.
-                modules={[Pagination, Navigation, Autoplay]}
-                className="mySwiper2"
-                autoplay ={{
-                    delay :2500,
-                    disableOnInteraction : false,
+                modules={[ Navigation ]}
+                // 스와이퍼 사이즈별 슬라이드수 변경!
+                breakpoints={{
+                    200: {
+                        slidesPerView: 3,
+                    },
+                    700: {
+                        slidesPerView: 4,
+                    },
+                    1000: {
+                        slidesPerView: 5,
+                    },
+                    1200: {
+                        slidesPerView: 7,
+                    },
                 }}
+                className="mySwiper2"
             >
                 {
-                    imgArr.map((v, i) =>
-                        <SwiperSlide key={i}>
-                            <img src={"./images/" + v + '.jpg'} alt="list image" />
-                        </SwiperSlide>
+                    selData.map((v, i) =>
+                        Number(v.idx) <= 7 && (
+                            <SwiperSlide key={i}>
+                                <img src={"./images/" + v + '.jpg'} alt="list image" />
+                            </SwiperSlide>
+                        )
                     )
                 }
             </Swiper>
