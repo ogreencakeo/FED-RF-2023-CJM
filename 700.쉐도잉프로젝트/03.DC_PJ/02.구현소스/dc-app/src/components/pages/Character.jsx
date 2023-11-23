6053025ab527b51e5cbbfbb106528c3683f74183
ogreencakeo <magente2000@gmail.com>
2023-11-23 13:15:23 +0900

    ASD

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Character.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Character.jsx
@@ -1,12 +1,20 @@
 // 캐릭터페이지 메인컨텐츠
 
 import { Banner } from "../modules/Banner";
+import { CatList } from "../modules/CatList";
 
-export function Character(/*props*/){
-    return(
+export function Character(/*props*/) {
+    return (
         <>
+            {/* 1. 배너 컴포넌트 */}
             <Banner category="CHARACTERS" />
             {/* <Banner category={props.cat} /> */}
+
+            {/* 2.  */}
+            
+
+            {/* 3. 캐릭터 리스트 컴포넌트 */}
+            <CatList />
         </>
     );
 } /////////////// Main 컴포넌트 ////////
