70fc413b886d6edfa61b267ce0945c51060dccc4
ogreencakeo <magente2000@gmail.com>
2023-11-23 10:32:44 +0900

    asd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -4,43 +4,47 @@ import { useEffect } from "react";
 import { Banner } from "../modules/Banner";
 
 // 자동스크롤 JS 불러오기
-import { autoScroll } from "../func/jquery-autoScroll";
+// import { autoScroll } from "../func/jquery-autoScroll";
 
-// 드래그 배너 JS 불러오기
+// 드래그배너 JS 불러오기
 import { dragBanner } from "../func/drag_banner";
 import { FashionIntro } from "../modules/FashionIntro";
-import { FooterArea } from "../layout/FooterArea";
 
 export function MainCont() {
+    // 메인 페이지일때만 자동스크롤 기능 적용함!
     useEffect(() => {
-        // 랜더링 후 한번만 적용
-        console.log("랜더링 OK!");
-        // 자동 스크롤 호출
-        autoScroll();
+        // 랜더링 후 한번만 적용!
+        // console.log("랜더링OK!");
 
-        // 드래그 배너 호출
+        //자동스크롤 호출
+        // autoScroll();
+
+        //드래그배너 호출
         dragBanner();
 
         // 컴포넌트 소멸자
-        return(()=>{
-            console.log('난 소멸했어~!');
-        }); // useEffect ///////////////
-    }, []); // useEffect
+        return () => {
+            console.log("난 소멸했어~!");
+        };
+    }, []); /////// useEffect ///////////
+
     return (
-        // 메인 페이지일때만 자동 스크롤 기능 적용함
         <>
             {/* 1. 배너 페이지 */}
             <section id="ban" className="page none-sel" style={{ background: "lightblue" }}>
                 <Banner />
             </section>
+
             {/* 2. 남성패션 페이지 */}
             <section className="page">
                 <FashionIntro cat="men" />
             </section>
+
             {/* 3. 여성패션 페이지 */}
             <section className="page">
                 <FashionIntro cat="women" />
             </section>
+
             {/* 4. 스타일패션 페이지 */}
             <section className="page">
                 <FashionIntro cat="style" />
@@ -73,4 +77,4 @@ export function MainCont() {
             </nav>
         </>
     );
-} // MainCont 컴포넌트 //////
+} //////// MainCont 컴포넌트 ///////
