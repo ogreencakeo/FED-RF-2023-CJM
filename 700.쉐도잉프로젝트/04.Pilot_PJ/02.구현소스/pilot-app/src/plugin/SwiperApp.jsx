7553f3bc41e897861ed08af313eebd2d381d0da8
ogreencakeo <magente2000@gmail.com>
2023-11-24 15:12:18 +0900

    Update SwiperApp.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/plugin/SwiperApp.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/plugin/SwiperApp.jsx
@@ -16,43 +16,30 @@ import "./css/swiper.css";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 export function SwiperApp(props) {
-    // 상태관리변수 : 멈춤상태 / 플레이 상태
-    const [sts, setSts] = useState(1);
+    // 리스트 만들기 함수 ////////////
+    const makeList = (num) => {
+        // num
+        let temp = [];
 
-    // useRef로 스와이퍼 객체를 받아온다.
-    const myRef = useRef(null);
-    // Swiper 컴포넌트의 ref속성에 담아서 연결!!!
+        for (let x=0; x<num; x++) {
+            temp[x] = (
+                <SwiperSlide>
+                    <img src={`../../images/sub/${props.cat}/banner/ban${x+1}.png`} alt="" />
+                </SwiperSlide>
+            );
+        }; /////////for ///////////
 
-    // 플레이 / 멈춤기능 함수
-    const stopPlay = () => {
-        console.log("멈추거나 플레이!");
+        // 배열을 리턴
+        return temp;
 
-        // sts 값이 1이면 멈춤
-        sts ? myRef.current.swiper.autoplay.stop() : myRef.current.swiper.autoplay.start();
-
-        // 상태값 업데이트 -> 컴포넌트 리랜더링!!!
-        sts ? setSts(0) : setSts(1);
-
-        console.log("리랜더링-> myFirst :", myFirst, ", mySecond.current : ", mySecond.current);
-    }; // stopPlay함수 //////////////
-
-    // 일반변수와 useRef 사용변수의 차이
-    let myFirst = "스와이퍼";
-    const mySecond = useRef("갤러리");
-
-    // 변수값 업데이트 함수
-    const myFn = () => {
-        myFirst = "Swiper";
-        mySecond.current = "Gallery";
-        console.log("함수호출-> myFirst :", myFirst, ", mySecond.current : ", mySecond.current);
-    }; // myFn 함수 ///////
+    }; // makeList 함수 ////////////
 
     // 리턴 코드 ///////////////////////////////
     return (
         <>
             <Swiper
                 // ref 속성에 useRef 할당 변수를 넣어서 외부에 연결함
-                ref={myRef}
+                // ref={myRef}
                 slidesPerView={1}
                 spaceBetween={0}
                 pagination={{
@@ -68,34 +55,8 @@ export function SwiperApp(props) {
                 modules={[Pagination, Navigation, Autoplay]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <img src={`../../images/sub/${props.cat}/banner/ban1.png`} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={`../../images/sub/${props.cat}/banner/ban2.png`} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={`../../images/sub/${props.cat}/banner/ban3.png`} alt="" />
-                </SwiperSlide>
+                {makeList(props.cat=='style'? 5 : 3)}
             </Swiper>
-            {/* 플레이/멈춤버튼 */}
-            <button
-                className="stopPlay"
-                style={{
-                    backgroundColor: "transparent",
-                    border: "none",
-                    fontSize: "40px",
-                    display: "block",
-                    margin: "0 auto",
-                    cursor: "pointer",
-                }}
-                title={sts ? "멈추기" : "자동넘기기"}
-                onClick={stopPlay}
-            >
-                {sts ? "▣" : "▶"}
-            </button>
-            {/* useRef 테스트 버튼 */}
-            <button onClick={myFn}>useRef 테스트</button>
         </>
     );
 } // SwiperApp 컴포넌트 //////////
