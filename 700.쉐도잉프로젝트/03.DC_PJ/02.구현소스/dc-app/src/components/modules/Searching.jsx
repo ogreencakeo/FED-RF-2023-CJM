2b6cf45fd35fae1582143d10ca933f23ce108fd4
ogreencakeo <magente2000@gmail.com>
2023-11-24 11:18:58 +0900

    Update Searching.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
@@ -6,6 +6,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export function Searching(props) {
     // props.kword - 검색어 전달
     console.log("Searching 컴포넌트 전달검색어 (props.kword) :", props.kword);
+
+    // 검색 리스트 만들기 함수
+    const schList = () => {};
+
+    // 엔터키 반응 함수
+    const enterKey = () => {};
+
+    // 체크박스 검색 함수 /////////
+    const chkSearch = () => {};
+
+    // 리턴 코드 ////////////////////
     return (
         <>
             {/* 전체 검색모듈 코드 */}
@@ -17,9 +28,33 @@ export function Searching(props) {
                     {/* 1-1. 검색 박스 */}
                     <div className="searching">
                         {/* 검색버튼 돋보기 아이콘 */}
-                        <FontAwesomeIcon icon={faSearch} className="schbtnGnb" title="Open Search" />
+                        <FontAwesomeIcon icon={faSearch} className="schbtn" title="Open Search" onClick={schList} />
                         {/* 입력창 */}
-                        <input id="schinGnb" type="text" placeholder="Filter by keyword" onKeyUp={enterKey} />
+                        <input id="schin" type="text" placeholder="Filter by keyword" onKeyUp={enterKey} />
+                    </div>
+                    {/* 1-2. 체크박스 구역 */}
+                    <div className="chkbx">
+                        <ul>
+                            <li>
+                                {/* 타이틀 */}
+                                <h2>
+                                    ALIGNMENT
+                                    <span className="spbtn">
+                                        ＋
+                                    </span>
+                                </h2>
+                                {/* 체크박스 리스트 */}
+                                <ol>
+                                    <li>
+                                        Heros
+                                        {/* 숨긴 체크박스 */}
+                                        <input type="checkbox" id="hero" className="chkhdn" onChange={chkSearch} />
+                                        {/* 디자인 노출 라벨 */}
+                                        <label htmlFor="hero" className="chklb"></label>
+                                    </li>   
+                                </ol>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </section>
