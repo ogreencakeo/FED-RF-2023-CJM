b0ac5b8e5d26b527ab23afb51aa71fcccd1b0ece
jimin0427 <cjm0427@hs.ac.kr>
2023-11-26 23:08:29 +0900

    sad

---
+++ pratice/my-first-react-app/src/component/modules/VidIntro.jsx
@@ -0,0 +1,41 @@
+import { vidIntroData } from "../data/vid_inrto";
+// 비디오 소개 CSS 불러오기
+import "../../css/vid_intro.css";
+
+export function VidIntro(props){
+    const selData = vidIntroData[props.cat];
+
+    const linkCode = (data) => {
+        return(
+            <>
+                {data.split('*')[0]}
+                <a href={selData.link[1]} target="_blank">
+                    {selData.link[0]}
+                </a>
+                {data.split('*')[1]}
+            </>
+        )
+    }
+    return(
+        <>
+            <section className="vidbox">
+                <div>
+                    <div className="vb1">
+                        <iframe src={selData.vsrc} title={selData.btit}></iframe>
+                    </div>
+                    <div className="vb2">
+                        <h3>{selData.stit}</h3>
+                        <h2>{selData.btit}</h2>
+                        <p>{selData.sum}</p>
+                        <p>
+                            {selData.desc.indexOf('*') == -1? selData.desc : linkCode(selData.desc)}
+                        </p>
+                        <p>
+                            {selData.sum.indexOf('*') == -1? selData.sum : linkCode(selData.sum)}
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
--- pratice/my-first-react-app/src/component/pages/Main.jsx
+++ pratice/my-first-react-app/src/component/pages/Main.jsx
@@ -1,11 +1,13 @@
 import { Banner } from "../modules/Banner";
 import { SecIntro } from "../modules/SecIntro";
+import { VidIntro } from "../modules/VidIntro";
 
 export function Main(){
     return(
         <>
             <Banner category ={`main${Math.ceil(Math.random()*3)}`} />
             <SecIntro />
+            <VidIntro cat='main' cls='off' />
         </>
     )
 }
