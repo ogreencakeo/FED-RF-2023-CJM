d16036583c174bf73c3d7518728ff97a586432b1
ogreencakeo <magente2000@gmail.com>
2023-11-28 11:17:53 +0900

    sad

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+
+import $ from "jquery";
+
 // 신상품 컴포넌트 /////////////////////
 export function SinSang(props) {
     // props.cat - 카테고리 분류명
 
     const makeList = () => {
+        // 코드 담을 배열
         let temp = [];
+        // 원하는 반복수 만큼 for문 실행하여 배열에 JSX태그 담기
         for (let x = 0; x < 9; x++) {
             temp[x] = (
                 <li className={`m${x + 1}`} key={x}>
@@ -13,8 +19,52 @@ export function SinSang(props) {
                 </li>
             );
         } // for //////////
+        // JSX 태그를 담은 배열을 리턴 -> 자동태그변환!
         return temp;
     }; // makeList 함수 ////////////////
+
+    // 신상품 리스트 이동함수 사용변수 ///////
+    // 위치값 변수 (left 값)
+    let lpos = 0;
+
+    // 재귀호출 상태값(1-호출, 0-멈춤)
+    let callStst = 1;
+
+    // 신상품 리스트 이동함수
+    const flowList = (ele) => {
+        // ele - 움직일 대상
+        // console.log("flowList ele :", ele);
+        // 대상의 left값을 1씩 감소함
+        lpos--;
+
+        // 이미지 박스 한개가 나가면 잘라서 맨뒤로 보냄
+        if (lpos < -300) {
+            // 위치값 초기화 (-301일때 0으로 변경!)
+            lpos = 0;
+            // 첫번째 li 맨뒤로 이동
+            ele.append(ele.find("li").first());
+        } // if /////////////
+
+        // 적용함
+        ele.css({ left: lpos + "px" });
+
+        // 재귀호출
+        if(callStst)
+            setTimeout(() => flowList(ele), 40);
+    }; // flowList /////////////////
+
+    // 오버 / 아웃시 이동제어 함수
+    const flowOut = () => {
+
+    }; 
+
+    // 랜더링 후 실행구역 //////////////
+    useEffect(() => {
+        // 대상선정 : .flist
+
+        // 신상품 리스트 이동 함수 호출!
+        flowList($(".flist"));
+    }); // useEffect ////////
 
     // 리턴 코드 ///////////////
     return (
@@ -23,10 +73,10 @@ export function SinSang(props) {
                 NEW MEN'S ARRIVAL
                 <button>전체리스트</button>
             </h2>
-            <div className="flowbx">
-                <ul className="flist">
-                    {makeList()}
-                </ul>
+            <div className="flowbx" 
+            onMouseOver={()=>callStst=0} 
+            onMouseOut={()=>callStst=1}>
+                <ul className="flist">{makeList()}</ul>
             </div>
         </>
     );
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -21,7 +21,7 @@ export function Fashion(props) {
 
     useEffect(() => {
         // 스크롤바 생성하기
-        $("html,body").css({ overflow: "visible" });
+        $("html,body").css({ overflow:"visible", overflowX:"hidden" });
 
         // 로고클릭시 페이지이동 : pgName 변경 -> chgPgName()
         $("#logo a").click(() => myCon.chgPgName("main"));
