dc14459d13cc4f9905dec6fd7263829a79d79c1e
ogreencakeo <magente2000@gmail.com>
2023-12-01 10:20:25 +0900

    Update Member.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
@@ -1,36 +1,25 @@
 // DC PJ 회원가입 페이지 컴포넌트
 
 // 회원가입 CSS 불러오기
-import '../../css/member.css';
+import "../../css/member.css";
 
-export function Member(){
-    return(
+export function Member() {
+    return (
         <>
             <div className="outbx">
                 <div className="membx">
                     <h2>Join Us</h2>
-                    <form action="process.php" method='post'>
+                    <form action="process.php" method="post">
                         <ul>
                             <li>
                                 {/* 아이디 */}
                                 <label>ID : </label>
-                                <input type="text" 
-                                    maxLength="20"
-                                    placeholder="Please enter your ID"
-                                />
+                                <input type="text" maxLength="20" placeholder="Please enter your ID" />
                             </li>
-                            <li>
-
-                            </li>
-                            <li>
-
-                            </li>
-                            <li>
-
-                            </li>
-                            <li>
-
-                            </li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
                             <li></li>
                             <li></li>
                             <li></li>
@@ -39,5 +28,5 @@ export function Member(){
                 </div>
             </div>
         </>
-    )
+    );
 } // Member 컴포넌트 /////////////
