0ccad113112214a8349cc13decf7b6237099f5af
ogreencakeo <magente2000@gmail.com>
2023-12-11 12:58:48 +0900

    Update Login.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
@@ -4,6 +4,9 @@
 import { useState } from "react";
 import "../../css/member.css";
 
+// 로컬스 데이터 초기화 함수
+import { initData } from "../func/mem_fn";
+
 // 제이쿼리
 import $ from "jquery";
 
@@ -20,17 +23,21 @@ export function Login() {
     const [userIdError, setUserIdError] = useState(false);
     // 2. 비밀번호변수
     const [pwdError, setPwdError] = useState(false);
+    
     // [ 아이디관련 메시지 프리셋 ] ////
     const msgId = [
         "This is a required entry", //필수입력
         "ID does not exist", //아이디가 존재하지 않습니다
         "Password doesn't match", //비밀번호가 일치하지 않습니다
     ];
+
     // [ 기타 메시지 프리셋 ]
     const msgEtc = {
         // 비밀번호
         pwd: "This is a required entry", //필수입력
     }; ///// msgEtc ///////
+
+
     // [3] 에러메시지 상태변수
     const [idMsg, setIdMsg] = useState(msgId[0]);
 
@@ -86,6 +93,46 @@ export function Login() {
         // 4-2. 유효성검사 전체 통과시 ////
         if (totalValid()) {
             console.log("통과!");
+            // DB역할의 로컬쓰에 데이터를 비교한다!
+            
+            // 만약 로컬스에 'mem-data'가 없으면 
+            // 초기화! -> 함수내에 이미 걸러내고 있음!
+            initData();
+
+            // 로컬스 'mem-data' 확인하기
+            let memData = localStorage.getItem('mem-data');
+
+            // 로컬스 데이터 객체화 하기
+            memData = JSON.parse(memData);
+            console.log(memData);
+
+            // 같은 아이디 검사 상태변수
+            // -> true이면 아이디 불일치 할 경우
+            let isNOT = true;
+
+            // 입력데이터 중 아이디 값 비교하기
+            // 배열 데이터 순회하며 값 비교하기
+            memData.forEach(v=>{
+                // 같은 아이디가 있는가?
+                if(v['uid'] === userId){
+                    console.log('아이디 같아요~!');
+                    // 아이디 에러 상태 업데이트
+                    setUserIdError(false);
+                    // 같은 아이디 상태 업데이트
+                    isNOT = false;
+                }
+            }); // forEach //////////
+
+            // 아이디가 불일치 할 경우
+            if(isNOT){
+                console.log('아이디 달라요~!');
+                // 아이디가 다를때 메시지 보이기
+                setIdMsg(msgId[1]);
+                // 아이디 에러 상태 업데이트
+                setUserIdError(true);
+
+            } // if //////////
+
         } ///// if ///////
         // 4-3. 유효성검사 불통과시 /////
         else {
