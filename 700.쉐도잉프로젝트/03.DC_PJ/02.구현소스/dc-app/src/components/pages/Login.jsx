b899ef5ba83c3ea9688c5fe7376740665f70ebbc
ogreencakeo <magente2000@gmail.com>
2023-12-12 10:53:40 +0900

    Update Login.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
@@ -23,7 +23,7 @@ export function Login() {
     const [userIdError, setUserIdError] = useState(false);
     // 2. 비밀번호변수
     const [pwdError, setPwdError] = useState(false);
-    
+
     // [ 아이디관련 메시지 프리셋 ] ////
     const msgId = [
         "This is a required entry", //필수입력
@@ -36,7 +36,6 @@ export function Login() {
         "This is a required entry", //필수입력
         "Password doesn't match", //비밀번호가 일치하지 않습니다
     ]; ///// msgEtc ///////
-
 
     // [3] 에러메시지 상태변수
     const [idMsg, setIdMsg] = useState(msgId[0]);
@@ -95,13 +94,13 @@ export function Login() {
         if (totalValid()) {
             console.log("통과!");
             // DB역할의 로컬쓰에 데이터를 비교한다!
-            
-            // 만약 로컬스에 'mem-data'가 없으면 
+
+            // 만약 로컬스에 'mem-data'가 없으면
             // 초기화! -> 함수내에 이미 걸러내고 있음!
             initData();
 
             // 로컬스 'mem-data' 확인하기
-            let memData = localStorage.getItem('mem-data');
+            let memData = localStorage.getItem("mem-data");
 
             // 로컬스 데이터 객체화 하기
             memData = JSON.parse(memData);
@@ -109,45 +108,94 @@ export function Login() {
 
             // 같은 아이디 검사 상태변수
             // -> true이면 아이디 불일치 할 경우
-            let isNOT = true;
+            // let isNOT = true; -> find() 사용시 불필요!
 
+            // ******************************************************
             // 입력데이터 중 아이디 값 비교하기
             // 배열 데이터 순회하며 값 비교하기
-            memData.forEach(v=>{
-                // 같은 아이디가 있는가?
-                if(v['uid'] === userId){
-                    console.log('아이디 같아요~!');
-                    // 아이디 에러 상태 업데이트
-                    setUserIdError(false);
-                    // 같은 아이디 상태 업데이트
-                    isNOT = false;
+            // 배열.find()로 검색순회하면 해당 데이터 만나는 순간
+            // 끝나고 나옴! -> 효율성을 높이자
+            let findD = memData.find((v) => {
+                if (v["uid"] === userId) return true;
+            }); // find /////////////
 
-                    // 비밀번호가 일치하는가?
-                    if(v['pwd'] === pwd) {
-                        console.log('비번이 같아요');
-                        // 비번 에러 상태값 업데이트
-                        setPwdError(false);
-                    } // if ///////
-                    else{ // 비번 불일치!
-                        console.log('비번 달라요~!');
-                        // 비번 다를때 메시지
-                        setPwdMsg(msgPwd[1]);
-                        // 비번 에러상태 업데이트
-                        setPwdError(true);
-                    } // else /////
-                } // if//////////
-            }); // forEach //////////
+            console.log("findD :", findD);
 
-            // 아이디가 불일치 할 경우
-            if(isNOT){
-                console.log('아이디 달라요~!');
+            // 만약 검색결과가 있으면 true 처리됨!
+            // 결과가 리턴이 없는 경우 undefined이므로 false
+            if (findD) {
+                // 같은 아이디가 있는 경우
+                console.log("아이디 같아요~!");
+                // 아이디 에러 상태 업데이트
+                setUserIdError(false);
+
+                // 비밀번호가 일치하는가? -> 로그인 최종성공!
+                if (findD["pwd"] === pwd) {
+                    console.log("비번이 같아요");
+                    // 비번 에러 상태값 업데이트
+                    setPwdError(false);
+
+                    // [ 로그인후 셋팅작업 ]
+                    // 1. 로그인한 회원정보를 로컬스에 셋팅
+                    // -> 서버의 세션을 대신하여 사용함
+                    localStorage.setItem('minfo', JSON.stringify(findD));
+                    
+                    // 2. 라우팅 페이지 이동하기 (useNavigate)
+
+                } // if ///////
+                else {
+                    // 비번 불일치!
+                    console.log("비번 달라요~!");
+                    // 비번 다를때 메시지
+                    setPwdMsg(msgPwd[1]);
+                    // 비번 에러상태 업데이트
+                    setPwdError(true);
+                } // else /////
+            } // if //////
+            else {
+                // 같은 아이디가 없는 경우
+                console.log("아이디 달라요~!");
                 // 아이디가 다를때 메시지 보이기
                 setIdMsg(msgId[1]);
                 // 아이디 에러 상태 업데이트
                 setUserIdError(true);
+            } // else ////
 
-            } // if //////////
+            // -> forEach를 사용하면 비효율적임 (모두 순회하므로!)
+            // memData.forEach((v) => {
+            //     // 같은 아이디가 있는가?
+            //     if (v["uid"] === userId) {
+            //         console.log("아이디 같아요~!");
+            //         // 아이디 에러 상태 업데이트
+            //         setUserIdError(false);
+            //         // 같은 아이디 상태 업데이트
+            //         isNOT = false;
 
+            //         // 비밀번호가 일치하는가?
+            //         if (v["pwd"] === pwd) {
+            //             console.log("비번이 같아요");
+            //             // 비번 에러 상태값 업데이트
+            //             setPwdError(false);
+            //         } // if ///////
+            //         else {
+            //             // 비번 불일치!
+            //             console.log("비번 달라요~!");
+            //             // 비번 다를때 메시지
+            //             setPwdMsg(msgPwd[1]);
+            //             // 비번 에러상태 업데이트
+            //             setPwdError(true);
+            //         } // else /////
+            //     } // if//////////
+            // }); // forEach //////////
+
+            // 아이디가 불일치 할 경우
+            // if (isNOT) {
+            //     console.log("아이디 달라요~!");
+            //     // 아이디가 다를때 메시지 보이기
+            //     setIdMsg(msgId[1]);
+            //     // 아이디 에러 상태 업데이트
+            //     setUserIdError(true);
+            // } // if //////////
         } ///// if ///////
         // 4-3. 유효성검사 불통과시 /////
         else {
@@ -217,7 +265,7 @@ export function Login() {
                             }
                         </li>
                         {/* 3.서브밋버튼 */}
-                        <li>
+                        <li style={{ overflow: "hidden" }}>
                             <button className="sbtn" onClick={onSubmit}>
                                 Submit
                             </button>
