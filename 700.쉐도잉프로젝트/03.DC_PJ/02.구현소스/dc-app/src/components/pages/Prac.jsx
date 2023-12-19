ffad19dec3f2710da24e7226b0024c3431ba0f4e
ogreencakeo <magente2000@gmail.com>
2023-12-19 12:20:15 +0900

    sdf

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Prac.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Prac.jsx
@@ -1,45 +1,7 @@
-import { useState } from "react";
-import { SchCatList } from "../modules/SchCatList";
-import { catListData } from "../data/swiper_cat";
-
-export function Seraching(props){
-    const [kwrod, setKword] = useState(null);
-    const [selData, setSelData] = useState(([], 2));
-    const [cnt, setCnt] = useState(0);
-
-    const chgKword = (txt) => setKword(txt);
-
-    const initFn = () => {
-        if(props.kwrod!=kwrod){
-            chgKword(props.kwrod);
-            schList();
-        }
-    }
-
-    initFn();
-
-    function schList(e){
-        let keyword = $('#schinGnb').val();
-        const newList = catListData.filter((v) => {
-            if(v.cname.indexOf(keyword)!=-1) return true;
-        });
-        setSelData([newList,2]);
-        setCnt(newList.length);
-    }
-
-
+export function ItemDetail(){
     return(
         <>
-            <div className="listbx">
-                <div className="restit">BROWSE CHARACTERS({cnt})</div>
-                <aside className="sortbx">
-                    <select name="sel" id="sel" className="sel" onChange={sortList}>
-                        <option value="0">A-Z</option>
-                        <option value="1">Z-A</option>
-                    </select>
-                </aside>
-                <SchCatList />
-            </div>
+            
         </>
-    )
+    );
 }
