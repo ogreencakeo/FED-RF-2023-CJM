abc03e0d42d9ef8693292ddc8ec5776f6fd8ce96
ogreencakeo <magente2000@gmail.com>
2023-12-21 16:02:51 +0900

    sadasd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Glist.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Glist.jsx
@@ -1,61 +1,105 @@
 // 상품 전체 리스트 페이지
 
 // 상품전체리스트 CSS 불러오기
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../css/glist.css";
 
-// 상품 데이터 불러오기
+// 제이쿼리
+import $ from "jquery";
+
+// 상품데이터 불러오기
 import gdata from "../data/glist-items";
 import { ItemDetail } from "../modules/ItemDetail";
 
-//정규식함수(숫자 세자리마다 콤마해주는 기능)
-function addComma(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-export function GList() {
-    // 상태관리 변수 /////////////////////////////////
-    // 1. 아이템 코드 (m1, m2, m3 ......)
-    const [item, setItem] = useState('m1');
-    // 2. 카테고리명 (men / wowen / style)
-    const [catName, setCatName] = useState('men');
-    // 3. 상세보기모드 (true / false)
-    const [detailSts, setDetailSts] = useState(true);
+console.log("전체Data:", gdata);
 
-    // 리스트 만들기 함수
-    const makeList = () => {
-        return gdata.map((v, i) => (
+export function GList() {
+    // 참조 변수 셋팅 : 리랜더링 없이 값 유지 /////////////////
+    // 1. 아이템 코드(m1,m2,m3,...)
+    const item = useRef("m1");
+    // 2. 카테고리명(men/women/style)
+    const catName = useRef("men");
+
+    // 리랜더링을 위한 상태변수 : 무조건 리랜더링 설정
+    const [force, setForce] = useState(null);
+
+    // 리스트 만들기 함수 ////////
+    const makeList = () =>
+        gdata.map((v, i) => (
             <div key={i}>
-                [{i + 1}]
-                <a href="#">
-                    <img src={`./images/goods/${v.cat}/${v.ginfo[0]}.png`} alt={v.ginfo[1]} />
+                <a
+                    href="#"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        showDetail(v.ginfo[0], v.cat);
+                    }}
+                >
+                    [{i + 1}]
+                    <img src={"./images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"} alt="dress" />
                     <aside>
                         <h2>{v.ginfo[1]}</h2>
                         <h3>{addComma(v.ginfo[3])}원</h3>
                     </aside>
                 </a>
             </div>
-        ));
-    }; // makeList //////////
-    // 리턴코드 /////////////////////
+        )); //////////// makeList ////////
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    //정규식함수(숫자 세자리마다 콤마해주는 기능)
+    function addComma(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    // 상품 클릭시 상세보기 보여주는 함수 /////
+    const showDetail = (gCode, catCode) => {
+        // gCode - 상품코드, catCode - 분류명
+        console.log(`상세보여! gCode : ${gCode}, catCode : ${catCode}`);
+
+        // 1. 값 업데이트
+        item.current = gCode;
+        catName.current = catCode;
+
+        // 2. 리랜더링 상태변경
+        setForce(Math.random());
+        // 리랜더링시 변경된 부분만 업데이트 한다!
+        // ItemDetail 컴포넌트 파트가 업데이트 됨
+
+        // 대상 보이기
+        $(".bgbx").slideDown(600);
+    }; // showDetail 함수 //////////
+
+    // 리턴 코드 ///////////////////
     return (
         <main id="cont">
+            <h1 className="tit">All ITEMS LIST</h1>
             <section>
                 <div id="optbx">
-                    <label htmlFor="men">남성</label> <input type="checkbox" id="men" />
-                    <label htmlFor="women">여성</label> <input type="checkbox" id="women" />
-                    <label htmlFor="style">스타일</label> <input type="checkbox" id="style" />
+                    <label htmlFor="men">남성</label>
+                    <input type="checkbox" id="men" defaultChecked />
+                    <label htmlFor="women">여성</label>
+                    <input type="checkbox" id="women" defaultChecked />
+                    <label htmlFor="style">스타일</label>
+                    <input type="checkbox" id="style" defaultChecked />
                 </div>
                 <div className="grid">{makeList()}</div>
             </section>
-            {
-                /* 2.5. 상세보기박스 */
-                detailSts && (
-                    <div className="bgbx">
-                        {/* <ItemDetail goods={item} cat={props.cat} /> */}
-                        <ItemDetail goods={item} cat={catName} />
-                    </div>
-                )
-            }
+            {/* 2.5. 상세보기박스 */}
+            <div
+                className="bgbx"
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    paddingTop: "12vh",
+                    backdropFilter: "blur(8px)",
+                    height: "100vh",
+                    zIndex: 9999,
+                }}
+            >
+                <ItemDetail goods={item.current} cat={catName.current} />
+            </div>
         </main>
     );
-} // Glist 컴포넌트 ////////////
+} /////////////// GList 컴포넌트 ///////////
