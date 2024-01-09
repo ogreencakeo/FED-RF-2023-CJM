6f7e0adf1def055523a73281ee79c58daedfbaf8
ogreencakeo <magente2000@gmail.com>
2024-01-09 19:07:24 +0900

    Update Banner.jsx

--- pratice/my-first-react-app/src/component/modules/Banner.jsx
+++ pratice/my-first-react-app/src/component/modules/Banner.jsx
@@ -6,6 +6,15 @@ export function Banner(props){
 
     const selData = banData[props.category];
 
+    const goSlide = (e) => {
+        const tg = e.target;
+        const sldBx = $(tg).siblings('.slider');
+        const indic = $(tg).siblings('.indic').find('li');
+        const sCnt = sldBx.find('li').length;
+
+
+    };
+
     const makeList = (data) => {
         return data.map((v, i) => (
             <li key={i}>
@@ -23,6 +32,21 @@ export function Banner(props){
     return(
         <div className="banner">
             <ul className="slider">{makeList(selData)}</ul>
+            {
+                selData.length > 1 && (
+                    <>
+                        <button className="abtn lb" onClick={goSlide}>＜</button>
+                        <button className="abtn rb" onClick={goSlide}>＞</button>
+                        <ol className="indic">
+                            {
+                                selData.map((v, i) => (
+                                    <li className={i==0? 'on' : '' } key={i}></li>
+                                ))
+                            }
+                        </ol>
+                    </>
+                )
+            }
         </div>
     );
 }
