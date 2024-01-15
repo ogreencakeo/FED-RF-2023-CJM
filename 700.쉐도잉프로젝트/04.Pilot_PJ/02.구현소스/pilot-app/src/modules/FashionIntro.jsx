7510ec77cd0689c0282a4ab409dcf0bed090fb1d
ogreencakeo <magente2000@gmail.com>
2024-01-15 10:48:13 +0900

    Update FashionIntro.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/FashionIntro.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/FashionIntro.jsx
@@ -11,6 +11,7 @@ import "../css/fashion_intro.css";
 
 export function FashionIntro(props) {
     // props.cat - 카테고리 분류명
+    // props.subcat - 서브 페이지 분류명
 
     // 컨텍스트 사용
     const myCon = useContext(pCon);
@@ -19,44 +20,57 @@ export function FashionIntro(props) {
     // 선택데이터
     const selData = fsData[props.cat];
 
+    // 만약 서브 페이지 데이터일 경우 다시 선택함
+    if (props.subcat !== "etc") {
+        // selData값을 덮어씀 : props.cat 값이 'sub'임
+        // props.subcat 값은 배열로 전달됨 : [카테고리, 순번]
+        selData = fsData[props.cat][props.subcat[0]][props.subcat[1]];
+        // 예컨데 fs['sub']['men'][0]
+    }
+
     // 리턴 코드
     return (
         <div id={props.cat} className="fs-page">
             <ul className="pgc" style={props.cat == "women" ? { flexDirection: "row-reverse" } : {}}>
                 {/* 스타일일때 이미지 경로는 배열 */}
-                <li className="imgc">
-                    <img
-                        src={props.cat == "style" ? selData.isrc[0] : selData.isrc}
-                        alt={props.cat == "style" ? selData.ialt[0] : selData.ialt}
-                    />
-                </li>
+                {/* 메인에서 호출하는 subcat이 etc인 경우만 해당!  */}
+                {props.subcat === "etc" && (
+                    <li className="imgc">
+                        <img
+                            src={props.cat == "style" ? selData.isrc[0] : selData.isrc}
+                            alt={props.cat == "style" ? selData.ialt[0] : selData.ialt}
+                        />
+                    </li>
+                )}
                 {/* 스타일이면 타이틀 2개, 아니면 1개 */}
-                <li className="txtc">
-                    {props.cat != "style" && (
-                        <h2>
-                            <a href="#" onClick={() => myCon.chgPgName(props.cat)}>
-                                {selData.tit[0]} <br />
-                                {selData.tit[1]}
-                            </a>
-                        </h2>
-                    )}
-                    {props.cat == "style" && (
-                        <>
-                            <h2 className="tm">
+                {props.cat === "etc" && (
+                    <li className="txtc">
+                        {props.cat != "style" && (
+                            <h2>
                                 <a href="#" onClick={() => myCon.chgPgName(props.cat)}>
-                                    {selData.tit[0][0]} <br />
-                                    {selData.tit[0][1]}
+                                    {selData.tit[0]} <br />
+                                    {selData.tit[1]}
                                 </a>
                             </h2>
-                            <h2 className="tw">
-                                <a href="#" onClick={() => myCon.chgPgName(props.cat)}>
-                                    {selData.tit[1][0]} <br />
-                                    {selData.tit[1][1]}
-                                </a>
-                            </h2>
-                        </>
-                    )}
-                </li>
+                        )}
+                        {props.cat == "style" && (
+                            <>
+                                <h2 className="tm">
+                                    <a href="#" onClick={() => myCon.chgPgName(props.cat)}>
+                                        {selData.tit[0][0]} <br />
+                                        {selData.tit[0][1]}
+                                    </a>
+                                </h2>
+                                <h2 className="tw">
+                                    <a href="#" onClick={() => myCon.chgPgName(props.cat)}>
+                                        {selData.tit[1][0]} <br />
+                                        {selData.tit[1][1]}
+                                    </a>
+                                </h2>
+                            </>
+                        )}
+                    </li>
+                )}
                 {/* 스타일 패션에서만 나오는 이미지 */}
                 {props.cat == "style" && (
                     <li className="imgc">
